5e70c5fc2835f01c2b1e054034d61a3842332424
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:42:46 +0000

    First commited as before_77723_rev2

--- src/integration/java/com/couchbase/client/java/SubdocumentExtendedAttributesTest.java
+++ src/integration/java/com/couchbase/client/java/SubdocumentExtendedAttributesTest.java
@@ -294,32 +294,4 @@ public class SubdocumentExtendedAttributesTest {
         assertTrue(result.cas() != 0);
         assertEquals(ResponseStatus.SUCCESS, result.status("spring.dict"));
     }
-
-    @Test
-    public void shouldAllowfullDocGetAndSetWithXattr() {
-        String key = "XattrWithFullDoc";
-        JsonObject content = JsonObject.create().put("foo", "bar");
-        DocumentFragment<Mutation> result = ctx.bucket()
-                .mutateIn(key)
-                .upsert("spring.class", "SomeClass", new SubdocOptionsBuilder().createParents(true).attributeAccess(true))
-                .upsert(content)
-                .createDocument(true)
-                .withExpiry(5)
-                .execute(PersistTo.ONE);
-
-        assertTrue(result.cas() != 0);
-        assertEquals(ResponseStatus.SUCCESS, result.status(0));
-        assertEquals(ResponseStatus.SUCCESS, result.status(1));
-
-        DocumentFragment<Lookup> lookupResult = ctx.bucket()
-                .lookupIn(key)
-                .get("spring.class", new SubdocOptionsBuilder().attributeAccess(true))
-                .get()
-                .execute();
-        assertTrue(result.cas() != 0);
-        assertEquals(ResponseStatus.SUCCESS, lookupResult.status(0));
-        assertEquals(ResponseStatus.SUCCESS, lookupResult.status(1));
-        assertEquals(content, lookupResult.content(1));
-
-    }
 }
