13b7b7d9143d4482200b1c324362d239f77adb5e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:52 +0000

    First commited as after_61226_rev1

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -483,6 +483,18 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
+        public DefaultCoreEnvironment.Builder callbacksOnIoPool(boolean callbacksOnIoPool) {
+            super.callbacksOnIoPool(callbacksOnIoPool);
+            return this;
+        }
+
+        @Override
+        public DefaultCoreEnvironment.Builder searchEndpoints(int searchEndpoints) {
+            super.searchEndpoints(searchEndpoints);
+            return this;
+        }
+
+        @Override
         public DefaultCouchbaseEnvironment build() {
             return new DefaultCouchbaseEnvironment(this);
         }
