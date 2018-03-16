ba9ea124201f05a4fbd83caa7d2ee56b1ddedce9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:53 +0000

    First commited as before_19677_rev3

--- src/test/java/com/couchbase/client/TestingClient.java
+++ src/test/java/com/couchbase/client/TestingClient.java
@@ -37,7 +37,6 @@ import net.spy.memcached.ops.OperationStatus;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpVersion;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
 import org.apache.http.message.BasicHttpRequest;
 
