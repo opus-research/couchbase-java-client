f83bd595e8619478ed68c89576d9adf1af29e00f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:31 +0000

    First commited as before_44015_rev3

--- src/main/java/com/couchbase/client/java/Bucket.java
+++ src/main/java/com/couchbase/client/java/Bucket.java
@@ -32,7 +32,9 @@ import com.couchbase.client.java.error.*;
 import com.couchbase.client.java.query.Query;
 import com.couchbase.client.java.query.QueryResult;
 import com.couchbase.client.java.transcoder.Transcoder;
-import com.couchbase.client.java.view.*;
+import com.couchbase.client.java.view.View;
+import com.couchbase.client.java.view.ViewQuery;
+import com.couchbase.client.java.view.ViewResult;
 import rx.Observable;
 
 import java.util.List;
@@ -1908,8 +1910,6 @@ public interface Bucket {
      */
     ViewResult query(ViewQuery query);
 
-    SpatialViewResult query(SpatialViewQuery query);
-
     /**
      * Queries a Couchbase Server {@link View} with a custom timeout.
      *
@@ -1926,8 +1926,6 @@ public interface Bucket {
      * @return a result containing all the found rows and additional information.
      */
     ViewResult query(ViewQuery query, long timeout, TimeUnit timeUnit);
-
-    SpatialViewResult query(SpatialViewQuery query, long timeout, TimeUnit timeUnit);
 
     /**
      * Experimental: Queries a N1QL secondary index with the default query timeout.
--- src/main/java/com/couchbase/client/java/view/ViewQuery.java
+++ src/main/java/com/couchbase/client/java/view/ViewQuery.java
@@ -51,11 +51,12 @@ public class ViewQuery {
     private static final int PARAM_ENDKEYDOCID_OFFSET = 26;
     private static final int PARAM_KEYS_OFFSET = 28;
     private static final int PARAM_KEY_OFFSET = 30;
+    private static final int PARAM_BBOX_OFFSET = 32;
 
     /**
      * Number of supported possible params for a query.
      */
-    private static final int NUM_PARAMS = 16;
+    private static final int NUM_PARAMS = 17;
 
     /**
      * Contains all stored params.
@@ -89,7 +90,7 @@ public class ViewQuery {
     }
 
     public ViewQuery development(boolean development) {
-        this.development = development;
+        this.development = true;
         return this;
     }
 
@@ -117,9 +118,6 @@ public class ViewQuery {
      * @return the {@link Query} object for proper chaining.
      */
     public ViewQuery limit(final int limit) {
-        if (limit < 0) {
-            throw new IllegalArgumentException("Limit must be >= 0.");
-        }
         params[PARAM_LIMIT_OFFSET] = "limit";
         params[PARAM_LIMIT_OFFSET+1] = Integer.toString(limit);
         return this;
@@ -186,9 +184,6 @@ public class ViewQuery {
      * @return the {@link Query} object for proper chaining.
      */
     public ViewQuery skip(final int skip) {
-        if (skip < 0) {
-            throw new IllegalArgumentException("Skip must be >= 0.");
-        }
         params[PARAM_SKIP_OFFSET] = "skip";
         params[PARAM_SKIP_OFFSET+1] = Integer.toString(skip);
         return this;
