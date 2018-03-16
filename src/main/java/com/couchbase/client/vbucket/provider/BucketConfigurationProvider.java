6f4d6ebf6c3273f6a9f2d7d522ed72b893cd3b19
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:38:00 +0000

    First commited as before_51153_rev2

--- src/main/java/com/couchbase/client/vbucket/provider/BucketConfigurationProvider.java
+++ src/main/java/com/couchbase/client/vbucket/provider/BucketConfigurationProvider.java
@@ -350,8 +350,8 @@ public class BucketConfigurationProvider extends SpyObject
       httpProvider.get().clearBuckets();
       Bucket config = httpProvider.get().getBucketConfiguration(bucket);
       setConfig(config);
-      bootstrapProvider = BootstrapProviderType.HTTP;
       monitorBucket();
+      bootstrapProvider = BootstrapProviderType.HTTP;
       return true;
     } catch(Exception ex) {
       getLogger().info("Could not fetch config from http seed nodes.", ex);
