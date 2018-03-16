7a350e447b94fee1ac9fca129a12462602363457
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:10 +0000

    First commited as after_57844_rev2

--- src/main/java/com/couchbase/client/java/query/consistency/ScanConsistency.java
+++ src/main/java/com/couchbase/client/java/query/consistency/ScanConsistency.java
@@ -21,6 +21,8 @@
  */
 package com.couchbase.client.java.query.consistency;
 
+import com.couchbase.client.core.annotations.InterfaceStability;
+
 /**
  * The possible values for scan_consistency in a N1QL request.
  *
@@ -32,12 +34,14 @@ public enum ScanConsistency {
     /**
      * This is the default (for single-statement requests). No timestamp vector is used
      * in the index scan.
+     *
      * This is also the fastest mode, because we avoid the cost of obtaining the vector,
      * and we also avoid any wait for the index to catch up to the vector.
      */
     NOT_BOUNDED,
     /**
      * This implements strong consistency per request.
+     *
      * Before processing the request, a current vector is obtained.
      * The vector is used as a lower bound for the statements in the request.
      * If there are DML statements in the request, RYOW is also applied within the request.
@@ -45,10 +49,20 @@ public enum ScanConsistency {
     REQUEST_PLUS,
     /**
      * This implements strong consistency per statement.
+     *
      * Before processing each statement, a current vector is obtained
      * and used as a lower bound for that statement.
      */
-    STATEMENT_PLUS;
+    STATEMENT_PLUS,
+    /**
+     * This implements bounded consistency.
+     *
+     * If this option used, additional mutation token need to be passed in which are used as a
+     * lower bound for that statement. This can be used to implement an optimized form of "read
+     * your own writes" over {@link ScanConsistency#REQUEST_PLUS}.
+     */
+    @InterfaceStability.Experimental
+    AT_PLUS;
 
     public String n1ql() {
         return this.name().toLowerCase();
