e59d67f2e87bdb4322ffed8906f394bc3d9f9b0a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:41:03 +0000

    First commited as before_64847_rev3

--- src/integration/java/com/couchbase/client/java/util/CouchbaseTestContext.java
+++ src/integration/java/com/couchbase/client/java/util/CouchbaseTestContext.java
@@ -152,7 +152,8 @@ public class CouchbaseTestContext {
             bucketPassword = TestProperties.password();
             bucketSettingsBuilder = DefaultBucketSettings
                 .builder()
-                .quota(100)
+                .quota(256)
+                .enableFlush(true)
                 .type(BucketType.COUCHBASE);
             flushOnInit = true;
             this.createAdhocBucket = false;
@@ -249,14 +250,6 @@ public class CouchbaseTestContext {
          */
         public Builder flushOnInit(boolean flushOnInit) {
             this.flushOnInit = flushOnInit;
-            return this;
-        }
-
-        /**
-         * Set to true deactivates flush capabilities on the bucket.
-         */
-        public Builder enableFlush(boolean enableFlush) {
-            this.bucketSettingsBuilder.enableFlush(enableFlush);
             return this;
         }
 
