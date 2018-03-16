96c6c9587e0b11c7fa1acf7e1eb8ce0efa70c7f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:27 +0000

    First commited as before_39265_rev2

--- src/integration/java/com/couchbase/client/java/util/ClusterDependentTest.java
+++ src/integration/java/com/couchbase/client/java/util/ClusterDependentTest.java
@@ -5,9 +5,6 @@ import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import rx.Observer;
-import rx.Subscriber;
-import rx.observables.BlockingObservable;
 
 /**
  * Base test class for tests that need a working cluster reference.
