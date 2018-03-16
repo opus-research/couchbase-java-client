194ba1111cda0b3e68d355d2202c1a09865b886f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:40:08 +0000

    First commited as after_62648_rev4

---
+++ src/main/java/com/couchbase/client/java/fts/facet/SearchFacet.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.java.fts.facet;
+
+
+import com.couchbase.client.java.document.json.JsonObject;
+
+public abstract class SearchFacet {
+
+    private final String name;
+    private final String field;
+    private final int limit;
+
+    protected SearchFacet(String name, String field, int limit) {
+        this.name = name;
+        this.field = field;
+        this.limit = limit;
+    }
+
+    public static SearchFacet term(String name, String field, int limit) {
+        return new TermFacet(name, field, limit);
+    }
+
+    public static SearchFacet numericRange(String name, String field, int limit, NumericRange... ranges) {
+        return new NumericRangeFacet(name, field, limit, ranges);
+    }
+
+    public static SearchFacet dateRange(String name, String field, int limit, DateRange... ranges) {
+        return new DateRangeFacet(name, field, limit, ranges);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void injectParams(JsonObject queryJson) {
+        queryJson.put("size", limit);
+        queryJson.put("field", field);
+    }
+
+}
