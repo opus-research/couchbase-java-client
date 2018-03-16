6971cb46f7844e3e90ebce80e9c7e707d2e59076
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:38:15 +0000

    First commited as after_53036_rev3

--- src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
+++ src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
@@ -62,6 +62,7 @@ import com.couchbase.client.core.message.query.GenericQueryRequest;
 import com.couchbase.client.core.message.query.GenericQueryResponse;
 import com.couchbase.client.core.message.view.ViewQueryRequest;
 import com.couchbase.client.core.message.view.ViewQueryResponse;
+import com.couchbase.client.core.retry.BestEffortRetryStrategy;
 import com.couchbase.client.core.utils.Buffers;
 import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
 import com.couchbase.client.java.bucket.AsyncBucketManager;
@@ -996,8 +997,10 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
                             public Observable<AsyncQueryResult> call(AsyncQueryRow row) {
                                 if (row != null && row.value().containsKey("operator")
                                         && row.value().getObject("operator").containsKey("#operator")) {
-                                    return Observable.error(new NamedPreparedStatementException("Named prepared statement "
-                                        + query.statement().preparedName() + " not found, it has been re-prepared"));
+                                    return Observable.error(
+                                            new NamedPreparedStatementException("Named prepared statement "
+                                                    + query.statement().preparedName()
+                                                    + " not found, it has been re-prepared"));
                                 }
 
                                 AsyncQueryResult copyResult = new DefaultAsyncQueryResult(cachedRows,
@@ -1007,6 +1010,20 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
                                 return Observable.just(copyResult);
                             }
                         });
+            }
+        })
+        .retry(new Func2<Integer, Throwable, Boolean>() {
+            @Override
+            public Boolean call(Integer attempts, Throwable throwable) {
+                int maxAttempts = 1;
+                if (environment().retryStrategy() == BestEffortRetryStrategy.INSTANCE) {
+                    maxAttempts = 100;
+                }
+                if (attempts <= maxAttempts && throwable instanceof NamedPreparedStatementException) {
+                    LOGGER.warn("Retrying #" + attempts + " prepared query " + query.n1ql());
+                    return true;
+                }
+                return false;
             }
         });
     }
