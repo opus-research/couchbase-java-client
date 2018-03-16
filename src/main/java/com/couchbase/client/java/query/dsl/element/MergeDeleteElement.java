2d148c85ec821d321b35ac20ef8b665043297d36
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:24 +0000

    First commited as before_58156_rev5

--- src/main/java/com/couchbase/client/java/query/dsl/element/MergeDeleteElement.java
+++
@@ -1,9 +0,0 @@
-package com.couchbase.client.java.query.dsl.element;
-
-public class MergeDeleteElement implements Element {
-
-  @Override
-  public String export() {
-    return "WHEN MATCHED THEN DELETE";
-  }
-}
--- src/main/java/com/couchbase/client/java/query/dsl/path/MergeDeletePath.java
+++
@@ -1,8 +0,0 @@
-package com.couchbase.client.java.query.dsl.path;
-
-
-public interface MergeDeletePath extends MergeInsertPath {
-
-  MergeDeleteWherePath whenMatchedThenDelete();
-
-}
