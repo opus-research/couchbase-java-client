ba03c279e2deb226a1c1b22d3de50765484fb336
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:56 +0000

    First commited as after_33607_rev4

--- src/test/java/com/couchbase/client/vbucket/BucketMonitorTest.java
+++ src/test/java/com/couchbase/client/vbucket/BucketMonitorTest.java
@@ -31,6 +31,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
 
 /**
