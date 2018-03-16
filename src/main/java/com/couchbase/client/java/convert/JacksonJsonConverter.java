31566f36516a358cd4ff5e44bdc7c82ed395f3e6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:25 +0000

    First commited as before_38881_rev5

--- src/main/java/com/couchbase/client/java/convert/JacksonJsonConverter.java
+++ src/main/java/com/couchbase/client/java/convert/JacksonJsonConverter.java
@@ -1,29 +1,6 @@
-/**
- * Copyright (C) 2014 Couchbase, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
- * IN THE SOFTWARE.
- */
 package com.couchbase.client.java.convert;
 
 import com.couchbase.client.core.message.ResponseStatus;
-import com.couchbase.client.core.message.document.CoreDocument;
-import com.couchbase.client.java.document.Document;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonArray;
 import com.couchbase.client.java.document.json.JsonObject;
@@ -48,7 +25,7 @@ import java.io.IOException;
 /**
  * Converter for {@link JsonObject}s.
  */
-public class JacksonJsonConverter implements Converter {
+public class JacksonJsonConverter implements Converter<JsonDocument, JsonObject> {
 
   /**
    * The internal jackson object mapper.
@@ -56,81 +33,81 @@ public class JacksonJsonConverter implements Converter {
   private final ObjectMapper mapper;
 
   public JacksonJsonConverter() {
-    final SimpleModule module = new SimpleModule("JsonValueModule", new Version(1, 0, 0, null, null, null));
+    mapper = new ObjectMapper();
+    SimpleModule module = new SimpleModule("JsonValueModule",
+      new Version(1, 0, 0, null, null, null));
     module.addSerializer(JsonObject.class, new JsonObjectSerializer());
     module.addSerializer(JsonArray.class, new JsonArraySerializer());
     module.addDeserializer(JsonObject.class, new JsonObjectDeserializer());
-
-    mapper = new ObjectMapper();
     mapper.registerModule(module);
   }
 
   @Override
-  public CoreDocument encode(final Document document) {
+  public JsonObject decode(ByteBuf buffer) {
     try {
-      final ByteBuf content = Unpooled.copiedBuffer(mapper.writeValueAsString(document.content()), CharsetUtil.UTF_8);
-      // TODO check if we should set the isJson flag to TRUE for JsonDocuments
-      return new CoreDocument(document.id(), content, 0, document.expiry(), document.cas(), false, document.status());
+      return mapper.readValue(buffer.toString(CharsetUtil.UTF_8),
+        JsonObject.class);
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @Override
+  public ByteBuf encode(JsonObject content) {
+    try {
+      return Unpooled.copiedBuffer(mapper.writeValueAsString(content),
+        CharsetUtil.UTF_8);
     } catch (JsonProcessingException e) {
       throw new IllegalStateException(e);
     }
   }
 
-  @Override
-  @SuppressWarnings("unchecked")
-  public <D extends Document<?>> D decode(final String id, final CoreDocument coreDocument, final ResponseStatus status) {
-    if (coreDocument == null) {
-      return (D) JsonDocument.create(id, null, 0, 0, status);
+    @Override
+    public JsonDocument newDocument(String id, JsonObject content, long cas, int expiry, ResponseStatus status) {
+        return JsonDocument.create(id, content, cas, expiry, status);
     }
 
-    try {
-      final JsonObject content = mapper.readValue(coreDocument.content().toString(CharsetUtil.UTF_8), JsonObject.class);
-      return (D) JsonDocument.create(id, content, coreDocument.cas(), coreDocument.expiration(), status);
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
-  }
-
-  public JsonObject decode(final String content) {
-    try {
-      return mapper.readValue(content, JsonObject.class);
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
-  }
-
+    /**
+   *
+   */
   static class JsonObjectSerializer extends JsonSerializer<JsonObject> {
     @Override
-    public void serialize(final JsonObject value, final JsonGenerator jsonGenerator, final SerializerProvider provider)
-        throws IOException {
-      jsonGenerator.writeObject(value.toMap());
+    public void serialize(JsonObject value, JsonGenerator jgen,
+      SerializerProvider provider) throws IOException {
+      jgen.writeObject(value.toMap());
     }
   }
 
+  /**
+   *
+   */
   static class JsonArraySerializer extends JsonSerializer<JsonArray> {
     @Override
-    public void serialize(final JsonArray value, final JsonGenerator jsonGenerator, final SerializerProvider provider)
-        throws IOException {
-      jsonGenerator.writeObject(value.toList());
+    public void serialize(JsonArray value, JsonGenerator jgen,
+      SerializerProvider provider) throws IOException {
+      jgen.writeObject(value.toList());
     }
   }
 
+  /**
+   *
+   */
   static class JsonObjectDeserializer extends JsonDeserializer<JsonObject> {
     @Override
-    public JsonObject deserialize(final JsonParser parser, final DeserializationContext ctx) throws IOException {
-      if (parser.getCurrentToken() == JsonToken.START_OBJECT) {
-        return decodeObject(parser, JsonObject.empty());
+    public JsonObject deserialize(JsonParser jp, DeserializationContext ctx)
+      throws IOException {
+      if (jp.getCurrentToken() == JsonToken.START_OBJECT) {
+        return decodeObject(jp, JsonObject.empty());
       } else {
         throw new IllegalStateException("Expecting Object as root level object, " +
-            "was: " + parser.getCurrentToken());
+          "was: " + jp.getCurrentToken());
       }
     }
 
     private JsonObject decodeObject(final JsonParser parser, final JsonObject target) throws IOException {
       JsonToken current = parser.nextToken();
       String field = null;
-
-      while (current != null && current != JsonToken.END_OBJECT) {
+      while(current != null && current != JsonToken.END_OBJECT) {
         if (current == JsonToken.START_OBJECT) {
           target.put(field, decodeObject(parser, JsonObject.empty()));
         } else if (current == JsonToken.START_ARRAY) {
@@ -138,7 +115,7 @@ public class JacksonJsonConverter implements Converter {
         } else if (current == JsonToken.FIELD_NAME) {
           field = parser.getCurrentName();
         } else {
-          switch (current) {
+          switch(current) {
             case VALUE_TRUE:
             case VALUE_FALSE:
               target.put(field, parser.getValueAsBoolean());
@@ -166,20 +143,18 @@ public class JacksonJsonConverter implements Converter {
 
         current = parser.nextToken();
       }
-
       return target;
     }
 
     private JsonArray decodeArray(final JsonParser parser, final JsonArray target) throws IOException {
       JsonToken current = parser.nextToken();
-
       while (current != null && current != JsonToken.END_ARRAY) {
         if (current == JsonToken.START_OBJECT) {
           target.add(decodeObject(parser, JsonObject.empty()));
         } else if (current == JsonToken.START_ARRAY) {
           target.add(decodeArray(parser, JsonArray.empty()));
         } else {
-          switch (current) {
+          switch(current) {
             case VALUE_TRUE:
             case VALUE_FALSE:
               target.add(parser.getValueAsBoolean());
@@ -207,7 +182,6 @@ public class JacksonJsonConverter implements Converter {
 
         current = parser.nextToken();
       }
-
       return target;
     }
 
--- src/main/java/com/couchbase/client/java/document/JsonDocument.java
+++ src/main/java/com/couchbase/client/java/document/JsonDocument.java
@@ -52,138 +52,134 @@ import com.couchbase.client.java.document.json.JsonObject;
  */
 public class JsonDocument extends AbstractDocument<JsonObject> {
 
-  /**
-   * Creates a empty {@link JsonDocument}.
-   *
-   * @return a empty {@link JsonDocument}.
-   */
-  public static JsonDocument empty() {
-    return new JsonDocument(null, null, 0, 0, null);
-  }
+    /**
+     * Creates a empty {@link JsonDocument}.
+     *
+     * @return a empty {@link JsonDocument}.
+     */
+    public static JsonDocument empty() {
+        return new JsonDocument(null, null, 0, 0, null);
+    }
 
-  /**
-   * Creates a {@link JsonDocument} which the document id.
-   *
-   * @param id the per-bucket unique document id.
-   * @return a {@link JsonDocument}.
-   */
-  public static JsonDocument create(String id) {
-    return new JsonDocument(id, null, 0, 0, null);
-  }
+    /**
+     * Creates a {@link JsonDocument} which the document id.
+     *
+     * @param id the per-bucket unique document id.
+     * @return a {@link JsonDocument}.
+     */
+    public static JsonDocument create(String id) {
+        return new JsonDocument(id, null, 0, 0, null);
+    }
 
-  /**
-   * Creates a {@link JsonDocument} which the document id and JSON content.
-   *
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @return a {@link JsonDocument}.
-   */
-  public static JsonDocument create(String id, JsonObject content) {
-    return new JsonDocument(id, content, 0, 0, null);
-  }
+    /**
+     * Creates a {@link JsonDocument} which the document id and JSON content.
+     *
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @return a {@link JsonDocument}.
+     */
+    public static JsonDocument create(String id, JsonObject content) {
+        return new JsonDocument(id, content, 0, 0, null);
+    }
 
-  /**
-   * Creates a {@link JsonDocument} which the document id, JSON content and the CAS value.
-   *
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @param cas     the CAS (compare and swap) value for optimistic concurrency.
-   * @return a {@link JsonDocument}.
-   */
-  public static JsonDocument create(String id, JsonObject content, long cas) {
-    return new JsonDocument(id, content, cas, 0, null);
-  }
+    /**
+     * Creates a {@link JsonDocument} which the document id, JSON content and the CAS value.
+     *
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @param cas the CAS (compare and swap) value for optimistic concurrency.
+     * @return a {@link JsonDocument}.
+     */
+    public static JsonDocument create(String id, JsonObject content, long cas) {
+        return new JsonDocument(id, content, cas, 0, null);
+    }
 
-  /**
-   * Creates a {@link JsonDocument} which the document id, JSON content and the expiration time.
-   *
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @param expiry  the expiration time of the document.
-   * @return a {@link JsonDocument}.
-   */
-  public static JsonDocument create(String id, JsonObject content, int expiry) {
-    return new JsonDocument(id, content, 0, expiry, null);
-  }
+    /**
+     * Creates a {@link JsonDocument} which the document id, JSON content and the expiration time.
+     *
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @param expiry the expiration time of the document.
+     * @return a {@link JsonDocument}.
+     */
+    public static JsonDocument create(String id, JsonObject content, int expiry) {
+        return new JsonDocument(id, content, 0, expiry, null);
+    }
 
-  /**
-   * Creates a {@link JsonDocument} which the document id, JSON content, CAS value, expiration time and status code.
-   *
-   * This factory method is normally only called within the client library when a response is analyzed and a document
-   * is returned which is enriched with the status code. It does not make sense to pre populate the status field from
-   * the user level code.
-   *
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @param cas     the CAS (compare and swap) value for optimistic concurrency.
-   * @param expiry  the expiration time of the document.
-   * @param status  the response status as returned by the underlying infrastructure.
-   * @return a {@link JsonDocument}.
-   */
-  public static JsonDocument create(String id, JsonObject content, long cas, int expiry, ResponseStatus status) {
-    return new JsonDocument(id, content, cas, expiry, status);
-  }
+    /**
+     * Creates a {@link JsonDocument} which the document id, JSON content, CAS value, expiration time and status code.
+     *
+     * This factory method is normally only called within the client library when a response is analyzed and a document
+     * is returned which is enriched with the status code. It does not make sense to pre populate the status field from
+     * the user level code.
+     *
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @param cas the CAS (compare and swap) value for optimistic concurrency.
+     * @param expiry the expiration time of the document.
+     * @param status the response status as returned by the underlying infrastructure.
+     * @return a {@link JsonDocument}.
+     */
+    public static JsonDocument create(String id, JsonObject content, long cas, int expiry, ResponseStatus status) {
+        return new JsonDocument(id, content, cas, expiry, status);
+    }
 
-  /**
-   * Creates a copy from a different {@link JsonDocument}, but changes the document ID.
-   *
-   * @param doc the original {@link JsonDocument} to copy.
-   * @param id  the per-bucket unique document id.
-   * @return a copied {@link JsonDocument} with the changed properties.
-   */
-  public static JsonDocument from(JsonDocument doc, String id) {
-    return JsonDocument.create(id, doc.content(), doc.cas(), doc.expiry(), doc.status());
-  }
+    /**
+     * Creates a copy from a different {@link JsonDocument}, but changes the document ID.
+     *
+     * @param doc the original {@link JsonDocument} to copy.
+     * @param id the per-bucket unique document id.
+     * @return a copied {@link JsonDocument} with the changed properties.
+     */
+    public static JsonDocument from(JsonDocument doc, String id) {
+        return JsonDocument.create(id, doc.content(), doc.cas(), doc.expiry(), doc.status());
+    }
 
-  /**
-   * Creates a copy from a different {@link JsonDocument}, but changes the content.
-   *
-   * @param doc     the original {@link JsonDocument} to copy.
-   * @param content the content of the document.
-   * @return a copied {@link JsonDocument} with the changed properties.
-   */
-  public static JsonDocument from(JsonDocument doc, JsonObject content) {
-    return JsonDocument.create(doc.id(), content, doc.cas(), doc.expiry(), doc.status());
-  }
+    /**
+     * Creates a copy from a different {@link JsonDocument}, but changes the content.
+     *
+     * @param doc the original {@link JsonDocument} to copy.
+     * @param content the content of the document.
+     * @return a copied {@link JsonDocument} with the changed properties.
+     */
+    public static JsonDocument from(JsonDocument doc, JsonObject content) {
+        return JsonDocument.create(doc.id(), content, doc.cas(), doc.expiry(), doc.status());
+    }
 
-  /**
-   * Creates a copy from a different {@link JsonDocument}, but changes the document ID and content.
-   *
-   * @param doc     the original {@link JsonDocument} to copy.
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @return a copied {@link JsonDocument} with the changed properties.
-   */
-  public static JsonDocument from(JsonDocument doc, String id, JsonObject content) {
-    return JsonDocument.create(id, content, doc.cas(), doc.expiry(), doc.status());
-  }
+    /**
+     * Creates a copy from a different {@link JsonDocument}, but changes the document ID and content.
+     *
+     * @param doc the original {@link JsonDocument} to copy.
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @return a copied {@link JsonDocument} with the changed properties.
+     */
+    public static JsonDocument from(JsonDocument doc, String id, JsonObject content) {
+        return JsonDocument.create(id, content, doc.cas(), doc.expiry(), doc.status());
+    }
 
-  /**
-   * Creates a copy from a different {@link JsonDocument}, but changes the CAS value.
-   *
-   * @param doc the original {@link JsonDocument} to copy.
-   * @param cas the CAS (compare and swap) value for optimistic concurrency.
-   * @return a copied {@link JsonDocument} with the changed properties.
-   */
-  public static JsonDocument from(JsonDocument doc, long cas) {
-    return JsonDocument.create(doc.id(), doc.content(), cas, doc.expiry(), doc.status());
-  }
+    /**
+     * Creates a copy from a different {@link JsonDocument}, but changes the CAS value.
+     *
+     * @param doc the original {@link JsonDocument} to copy.
+     * @param cas the CAS (compare and swap) value for optimistic concurrency.
+     * @return a copied {@link JsonDocument} with the changed properties.
+     */
+    public static JsonDocument from(JsonDocument doc, long cas) {
+        return JsonDocument.create(doc.id(), doc.content(), cas, doc.expiry(), doc.status());
+    }
 
-  @Override
-  public Document<JsonObject> copy(final long cas, final ResponseStatus status) {
-    return new JsonDocument(id(), content(), cas, expiry(), status);
-  }
+    /**
+     * Private constructor which is called by the static factory methods eventually.
+     *
+     * @param id the per-bucket unique document id.
+     * @param content the content of the document.
+     * @param cas the CAS (compare and swap) value for optimistic concurrency.
+     * @param expiry the expiration time of the document.
+     * @param status the response status as returned by the underlying infrastructure.
+     */
+    private JsonDocument(String id, JsonObject content, long cas, int expiry, ResponseStatus status) {
+        super(id, content, cas, expiry, status);
+    }
 
-  /**
-   * Private constructor which is called by the static factory methods eventually.
-   *
-   * @param id      the per-bucket unique document id.
-   * @param content the content of the document.
-   * @param cas     the CAS (compare and swap) value for optimistic concurrency.
-   * @param expiry  the expiration time of the document.
-   * @param status  the response status as returned by the underlying infrastructure.
-   */
-  private JsonDocument(String id, JsonObject content, long cas, int expiry, ResponseStatus status) {
-    super(id, content, cas, expiry, status);
-  }
 }
