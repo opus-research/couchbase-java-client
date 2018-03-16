292319613ee5b5d31c2bc4e4106b73028e35702c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:17 +0000

    First commited as before_46690_rev2

--- src/main/java/com/couchbase/client/java/transcoder/JsonArrayTranscoder.java
+++ src/main/java/com/couchbase/client/java/transcoder/JsonArrayTranscoder.java
@@ -78,16 +78,13 @@ public class JsonArrayTranscoder extends AbstractTranscoder<JsonArrayDocument, J
 
     public JsonArray byteBufToJsonArray(ByteBuf input) throws Exception {
         byte[] inputBytes;
-        int offset = 0;
-        int length = input.readableBytes();
         if (input.hasArray()) {
             inputBytes = input.array();
-            offset = input.arrayOffset() + input.readerIndex();
         } else {
-            inputBytes = new byte[length];
-            input.getBytes(input.readerIndex(), inputBytes);
+            inputBytes = new byte[input.readableBytes()];
+            input.getBytes(0, inputBytes);
         }
-        return JacksonTransformers.MAPPER.readValue(inputBytes, offset, length, JsonArray.class);
+        return JacksonTransformers.MAPPER.readValue(inputBytes, JsonArray.class);
     }
 
 }
--- src/main/java/com/couchbase/client/java/transcoder/JsonTranscoder.java
+++ src/main/java/com/couchbase/client/java/transcoder/JsonTranscoder.java
@@ -87,16 +87,13 @@ public class JsonTranscoder extends AbstractTranscoder<JsonDocument, JsonObject>
      */
     public JsonObject byteBufToJsonObject(ByteBuf input) throws Exception {
         byte[] inputBytes;
-        int offset = 0;
-        int length = input.readableBytes();
         if (input.hasArray()) {
             inputBytes = input.array();
-            offset = input.arrayOffset() + input.readerIndex();
         } else {
-            inputBytes = new byte[length];
-            input.getBytes(input.readerIndex(), inputBytes);
+            inputBytes = new byte[input.readableBytes()];
+            input.getBytes(0, inputBytes);
         }
-        return JacksonTransformers.MAPPER.readValue(inputBytes, offset, length, JsonObject.class);
+        return JacksonTransformers.MAPPER.readValue(inputBytes, JsonObject.class);
     }
 
 
