8076cae8d32ac976d27007e0834aaafc9752dcb7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:38:42 +0000

    First commited as after_55845_rev2

---
+++ src/main/java/com/couchbase/client/java/search/IndexParams.java
@@ -0,0 +1,10 @@
+package com.couchbase.client.java.search;
+
+import com.couchbase.client.java.document.json.JsonObject;
+
+/**
+ * @author Sergey Avseyev
+ */
+public interface IndexParams {
+    JsonObject json();
+}
