67f8242220a80c890f15874c168159594992e16c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:36 +0000

    First commited as before_49377_rev7

--- src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
+++ src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
@@ -85,9 +85,8 @@ import com.couchbase.client.java.query.Query;
 import com.couchbase.client.java.query.QueryPlan;
 import com.couchbase.client.java.query.SimpleQuery;
 import com.couchbase.client.java.query.Statement;
-import com.couchbase.client.java.repository.AsyncRepository;
-import com.couchbase.client.java.repository.CouchbaseAsyncRepository;
 import com.couchbase.client.java.transcoder.BinaryTranscoder;
+import com.couchbase.client.java.transcoder.JacksonTransformers;
 import com.couchbase.client.java.transcoder.JsonArrayTranscoder;
 import com.couchbase.client.java.transcoder.JsonBooleanTranscoder;
 import com.couchbase.client.java.transcoder.JsonDoubleTranscoder;
@@ -174,11 +173,6 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
     @Override
     public Observable<ClusterFacade> core() {
         return Observable.just(core);
-    }
-
-    @Override
-    public AsyncRepository repository() {
-        return new CouchbaseAsyncRepository(this);
     }
 
     @Override
