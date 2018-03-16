98d1339a129b4dc094109ac021da87512b1e2be9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:47 +0000

    First commited as before_45000_rev3

--- src/main/java/com/couchbase/client/java/query/ParametrizedQuery.java
+++ src/main/java/com/couchbase/client/java/query/ParametrizedQuery.java
@@ -21,6 +21,8 @@
  */
 package com.couchbase.client.java.query;
 
+import java.util.Map;
+
 import com.couchbase.client.java.document.json.JsonArray;
 import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.document.json.JsonValue;
@@ -39,7 +41,7 @@ import com.couchbase.client.java.document.json.JsonValue;
  * @author Simon Baslé
  * @since 2.1
  */
-public class ParametrizedQuery extends AbstractQuery {
+public class ParametrizedQuery implements Query {
 
     private final Statement statement;
     private final JsonValue params;
@@ -70,23 +72,33 @@ public class ParametrizedQuery extends AbstractQuery {
         this.params = namedParams;
     }
 
+    protected String statementType() {
+        return "statement";
+    }
+
     @Override
     public Statement statement() {
         return this.statement;
     }
 
     @Override
-    protected String statementType() {
-        return "statement";
+    public String toN1QL() {
+        JsonObject query = JsonObject.create()
+                .put(statementType(), statement.toString());
+        if (params instanceof JsonArray && !((JsonArray) params).isEmpty()) {
+           query.put("args", (JsonArray) params);
+        } else if (params instanceof JsonObject && !((JsonObject) params).isEmpty()) {
+            JsonObject namedParams = (JsonObject) params;
+            for (String key : namedParams.getNames()) {
+                Object value = namedParams.get(key);
+                if (key.charAt(0) != '$') {
+                    query.put('$' + key, value);
+                } else {
+                    query.put(key, value);
+                }
+            }
+        } //else do nothing, as if a simple statement
+        return query.toString(); //return json-escaped string
     }
 
-    @Override
-    protected Object statementValue() {
-        return statement.toString();
-    }
-
-    @Override
-    protected JsonValue statementParameters() {
-        return params;
-    }
 }
