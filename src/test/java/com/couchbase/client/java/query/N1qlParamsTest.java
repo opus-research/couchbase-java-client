c1892d377533e81290e35e303918d1582499fe60
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:43:07 +0000

    First commited as after_81842_rev6

--- src/test/java/com/couchbase/client/java/query/N1qlParamsTest.java
+++ src/test/java/com/couchbase/client/java/query/N1qlParamsTest.java
@@ -25,7 +25,9 @@ import com.couchbase.client.java.query.consistency.ScanConsistency;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
