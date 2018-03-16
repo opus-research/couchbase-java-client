85f1fd498b161ef03ca7463d06c136e3c21049e8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:47 +0000

    First commited as before_32306_rev4

--- src/test/java/com/couchbase/client/ViewTest.java
+++ src/test/java/com/couchbase/client/ViewTest.java
@@ -55,12 +55,10 @@ import java.util.Map.Entry;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.spy.memcached.PersistTo;
 import net.spy.memcached.TestConfig;
-import net.spy.memcached.internal.OperationFuture;
 import net.spy.memcached.ops.OperationStatus;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -169,14 +167,9 @@ public class ViewTest {
     String view2 = "{\"language\":\"javascript\",\"views\":{\""
         + VIEW_NAME_FOR_DATED + "\":{\"map\":\"function (doc) {  "
         + "emit(doc.type, 1)}\"}}}";
-
     for (Entry<String, Object> item : ITEMS.entrySet()) {
-      OperationFuture<Boolean> future = client.set(item.getKey(),
-        item.getValue(), PersistTo.MASTER);
-      assertTrue(future.getStatus().toString(),
-        future.get(1, TimeUnit.MINUTES));
+      assert client.set(item.getKey(), 0, item.getValue()).get().booleanValue();
     }
-
     HttpFuture<String> asyncHttpPut = client.asyncHttpPut(docUri, view2);
 
     String response = asyncHttpPut.get();
@@ -234,6 +227,17 @@ public class ViewTest {
   private static String generateDatedDoc(int year, int month, int day) {
     return "{\"type\":\"dated\",\"year\":" + year + ",\"month\":" + month + ","
         + "\"day\":" + day + "}";
+  }
+
+  @Test
+  public void testAssertions() {
+    boolean caught = false;
+    try {
+      assert false;
+    } catch (AssertionError e) {
+      caught = true;
+    }
+    assertTrue("Assertions are not enabled!", caught);
   }
 
   /**
@@ -309,7 +313,6 @@ public class ViewTest {
       client.asyncGetView(DESIGN_DOC_W_REDUCE, VIEW_NAME_W_REDUCE);
 
     final CountDownLatch latch = new CountDownLatch(1);
-    final AtomicInteger callCount = new AtomicInteger(0);
     future.addListener(new HttpCompletionListener() {
       @Override
       public void onComplete(HttpFuture<?> f) throws Exception {
@@ -320,7 +323,6 @@ public class ViewTest {
           public void onComplete(HttpFuture<?> f) throws Exception {
             ViewResponse resp = (ViewResponse) f.get();
             if (resp.size() == ITEMS.size()) {
-              callCount.incrementAndGet();
               latch.countDown();
             }
           }
@@ -329,7 +331,6 @@ public class ViewTest {
     });
 
     assertTrue(latch.await(3, TimeUnit.SECONDS));
-    assertEquals(1, callCount.get());
   }
 
   @Test
@@ -943,12 +944,11 @@ public class ViewTest {
    */
   @Test
   public void testObserveWithStaleFalse()
-    throws Exception {
+    throws InterruptedException, ExecutionException {
     int docAmount = 500;
     for (int i = 1; i <= docAmount; i++) {
       String value = "{\"type\":\"observetest\",\"value\":"+i+"}";
-      assertTrue(client.set("observetest"+i, 0, value, PersistTo.MASTER)
-        .get(20, TimeUnit.SECONDS));
+      assertTrue(client.set("observetest"+i, 0, value, PersistTo.MASTER).get());
     }
 
     Query query = new Query().setStale(Stale.FALSE);
@@ -980,11 +980,11 @@ public class ViewTest {
    * non-JSON data is read from the view.
    */
   @Test
-  public void testViewWithBinaryDocs() throws Exception {
+  public void testViewWithBinaryDocs() {
     // Create non-JSON documents
     Date now = new Date();
-    client.set("nonjson1", 0, now, PersistTo.MASTER).get(20, TimeUnit.SECONDS);
-    client.set("nonjson2", 0, 42, PersistTo.MASTER).get(20, TimeUnit.SECONDS);
+    client.set("nonjson1", 0, now);
+    client.set("nonjson2", 0, 42);
 
     View view = client.getView(DESIGN_DOC_BINARY, VIEW_NAME_BINARY);
     Query query = new Query();
