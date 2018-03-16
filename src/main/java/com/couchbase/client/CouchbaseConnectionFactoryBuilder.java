fe7744206405a2a7c1e00d010f0c8b8a77ea3d7e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:22 +0000

    First commited as before_28933_rev1

--- src/main/java/com/couchbase/client/CouchbaseConnectionFactoryBuilder.java
+++ src/main/java/com/couchbase/client/CouchbaseConnectionFactoryBuilder.java
@@ -22,7 +22,6 @@
 
 package com.couchbase.client;
 
-import com.couchbase.client.vbucket.CouchbaseNodeOrder;
 import com.couchbase.client.vbucket.config.Config;
 
 import java.io.IOException;
@@ -56,8 +55,6 @@ public class CouchbaseConnectionFactoryBuilder extends ConnectionFactoryBuilder{
     CouchbaseConnectionFactory.DEFAULT_OBS_POLL_INTERVAL;
   private int obsPollMax = CouchbaseConnectionFactory.DEFAULT_OBS_POLL_MAX;
   private int viewTimeout = CouchbaseConnectionFactory.DEFAULT_VIEW_TIMEOUT;
-  private CouchbaseNodeOrder nodeOrder
-    = CouchbaseConnectionFactory.DEFAULT_STREAMING_NODE_ORDER;
   private static final Logger LOGGER =
     Logger.getLogger(CouchbaseConnectionFactoryBuilder.class.getName());
 
@@ -93,17 +90,6 @@ public class CouchbaseConnectionFactoryBuilder extends ConnectionFactoryBuilder{
         + "more than 2500ms.");
     }
     viewTimeout = timeout;
-    return this;
-  }
-
-  /**
-   * Set the streaming connection node ordering.
-   *
-   * @param order the ordering to use.
-   * @return the builder for chaining.
-   */
-  public CouchbaseConnectionFactoryBuilder setStreamingNodeOrder(CouchbaseNodeOrder order) {
-    nodeOrder = order;
     return this;
   }
 
