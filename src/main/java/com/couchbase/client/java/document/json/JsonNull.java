4aae7fd570be3cae9c8f2876845e4ab3acbe4504
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:51 +0000

    First commited as before_41131_rev6

--- src/main/java/com/couchbase/client/java/document/json/JsonNull.java
+++
@@ -1,9 +0,0 @@
-package com.couchbase.client.java.document.json;
-
-public class JsonNull extends JsonValue {
-
-    public static JsonNull INSTANCE = new JsonNull();
-
-    private JsonNull() {}
-
-}
