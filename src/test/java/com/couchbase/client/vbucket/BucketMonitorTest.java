6c53ca34f7d0421c8f137f0cf96d782cc11efb7e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:27 +0000

    First commited as before_29539_rev2

--- src/test/java/com/couchbase/client/vbucket/BucketMonitorTest.java
+++ src/test/java/com/couchbase/client/vbucket/BucketMonitorTest.java
@@ -23,17 +23,17 @@
 package com.couchbase.client.vbucket;
 
 import com.couchbase.client.vbucket.config.ConfigurationParserMock;
-import java.net.URI;
-import net.spy.memcached.TestConfig;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.net.URI;
+
+import junit.framework.TestCase;
+
+import net.spy.memcached.TestConfig;
 
 /**
  * A BucketMonitorTest.
  */
-public class BucketMonitorTest {
+public class BucketMonitorTest extends TestCase {
   private static final String USERNAME = "default";
   private static final String PASSWORD = "";
   private static final String STREAMING_URI = "http://" + TestConfig.IPV4_ADDR
@@ -51,8 +51,8 @@ public class BucketMonitorTest {
    * password match with those configured.
    * @throws Exception
    */
-  @Test
   public void testInstantiate() throws Exception {
+
     BucketMonitor bucketMonitor = new BucketMonitor(new URI(STREAMING_URI),
         BUCKET_NAME, USERNAME, PASSWORD, CONFIG_PARSER);
     assertEquals(USERNAME, bucketMonitor.getHttpUser());
@@ -71,7 +71,6 @@ public class BucketMonitorTest {
    * Shutdown the bucket monitor.
    * @throws Exception
    */
-  @Test
   public void testObservable() throws Exception {
     BucketMonitor bucketMonitor = new BucketMonitor(new URI(STREAMING_URI),
         BUCKET_NAME, USERNAME, PASSWORD, CONFIG_PARSER);
@@ -85,17 +84,4 @@ public class BucketMonitorTest {
         observer.isUpdateCalled());
     bucketMonitor.shutdown();
   }
-
-  /**
-   * Tests failing on invalid hostname.
-   *
-   * @throws Exception
-   */
-  @Test(expected = ConnectionException.class)
-  public void shouldFailOnInvalidPeer() throws Exception {
-    BucketMonitor monitor = new BucketMonitor(new URI("http://invalidHost:8091/"),
-      BUCKET_NAME, USERNAME, PASSWORD, CONFIG_PARSER);
-    monitor.startMonitor();
-  }
-
 }
