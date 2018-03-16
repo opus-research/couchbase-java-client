a6c41eb03bf5e7c9f0717e89c9d1e5c5480bc234
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:55 +0000

    First commited as before_19694_rev1

--- src/test/java/com/couchbase/client/CbTestConfig.java
+++ src/test/java/com/couchbase/client/CbTestConfig.java
@@ -28,7 +28,7 @@ package com.couchbase.client;
 public final class CbTestConfig {
   public static final String CLUSTER_PASS_PROP = "cluster.password";
   public static final String CLUSTER_ADMINNAME_PROP = "cluster.adminname";
-  public static final String CLUSTER_PASS =
+  public static final String CLUSTER_PASS = 
     System.getProperty(CLUSTER_PASS_PROP, "password");
   public static final String CLUSTER_ADMINNAME =
     System.getProperty(CLUSTER_ADMINNAME_PROP, "Administrator");
