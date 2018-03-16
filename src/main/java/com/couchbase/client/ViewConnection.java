3abc29acc4326719ad783c2154e1e03bba05f50b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:32 +0000

    First commited as after_13688_rev1

--- src/main/java/com/couchbase/client/ViewConnection.java
+++ src/main/java/com/couchbase/client/ViewConnection.java
@@ -27,23 +27,18 @@ import com.couchbase.client.ViewNode;
 import com.couchbase.client.ViewNode.EventLogger;
 import com.couchbase.client.ViewNode.MyHttpRequestExecutionHandler;
 import com.couchbase.client.http.AsyncConnectionManager;
+import com.couchbase.client.protocol.views.HttpOperation;
 import com.couchbase.client.vbucket.Reconfigurable;
-import com.couchbase.client.vbucket.VBucketNodeLocator;
 import com.couchbase.client.vbucket.config.Bucket;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.nio.channels.CancelledKeyException;
-import java.nio.channels.ClosedSelectorException;
-import java.nio.channels.Selector;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.http.HttpHost;
@@ -65,33 +60,36 @@ import org.apache.http.protocol.RequestUserAgent;
 
 import net.spy.memcached.AddrUtil;
 import net.spy.memcached.ConnectionObserver;
-import net.spy.memcached.FailureMode;
-import net.spy.memcached.MemcachedConnection;
-import net.spy.memcached.MemcachedNode;
-import net.spy.memcached.ops.KeyedOperation;
-import net.spy.memcached.ops.Operation;
-import net.spy.memcached.ops.VBucketAware;
+import net.spy.memcached.compat.SpyThread;
 
 /**
  * Couchbase implementation of CouchbaseConnection.
  *
  */
-public class CouchbaseConnection extends MemcachedConnection  implements
+public class ViewConnection extends SpyThread  implements
   Reconfigurable {
   private static final int NUM_CONNS = 1;
 
+  private volatile boolean shutDown = false;
+  protected volatile boolean reconfiguring = false;
+  protected volatile boolean running = true;
+
   private final CouchbaseConnectionFactory connFactory;
   private final ConcurrentLinkedQueue<ViewNode> nodesToShutdown;
-  private List<ViewNode> nodes;
+  private final Collection<ConnectionObserver> connObservers =
+      new ConcurrentLinkedQueue<ConnectionObserver>();
+  private List<ViewNode> couchNodes;
+  private int nextNode;
 
-  public CouchbaseConnection(CouchbaseConnectionFactory cf,
+  public ViewConnection(CouchbaseConnectionFactory cf,
       List<InetSocketAddress> addrs, Collection<ConnectionObserver> obs)
     throws IOException {
-    super(cf.getReadBufSize(), cf, addrs, obs, cf.getFailureMode(), cf.getOperationFactory());
-    shutDown = false;
     connFactory = cf;
     nodesToShutdown = new ConcurrentLinkedQueue<ViewNode>();
-    nodes = createConnections(addrs);
+    connObservers.addAll(obs);
+    couchNodes = createConnections(addrs);
+    nextNode = 0;
+    start();
   }
 
   private List<ViewNode> createConnections(List<InetSocketAddress> addrs)
@@ -136,7 +134,65 @@ public class CouchbaseConnection extends MemcachedConnection  implements
   return nodeList;
 }
 
-  protected volatile boolean reconfiguring = false;
+  public void addOp(final HttpOperation op) {
+    couchNodes.get(getNextNode()).addOp(op);
+  }
+
+  public void handleIO() {
+    for (ViewNode node : couchNodes) {
+      node.doWrites();
+    }
+
+    for (ViewNode qa : nodesToShutdown) {
+      nodesToShutdown.remove(qa);
+      Collection<HttpOperation> notCompletedOperations = qa.destroyWriteQueue();
+      try {
+        qa.shutdown();
+      } catch (IOException e) {
+        getLogger().error("Error shutting down connection to "
+            + qa.getSocketAddress());
+      }
+      redistributeOperations(notCompletedOperations);
+    }
+  }
+
+  private void redistributeOperations(Collection<HttpOperation> ops) {
+    int added = 0;
+    for (HttpOperation op : ops) {
+      addOp(op);
+      added++;
+    }
+    assert added > 0 : "Didn't add any new operations when redistributing";
+  }
+
+  private int getNextNode() {
+    return nextNode = (++nextNode % couchNodes.size());
+  }
+
+  protected void checkState() {
+    if (shutDown) {
+      throw new IllegalStateException("Shutting down");
+    }
+    assert isAlive();
+  }
+
+  public boolean shutdown() throws IOException {
+    if (shutDown) {
+      getLogger().info("Suppressing duplicate attempt to shut down");
+      return false;
+    }
+    shutDown = true;
+    running = false;
+    for (ViewNode n : couchNodes) {
+      if (n != null) {
+        n.shutdown();
+        if (n.hasWriteOps()) {
+          getLogger().warn("Shutting down with ops waiting to be written");
+        }
+      }
+    }
+    return true;
+  }
 
   public void reconfigure(Bucket bucket) {
     reconfiguring = true;
@@ -155,7 +211,7 @@ public class CouchbaseConnection extends MemcachedConnection  implements
       ArrayList<ViewNode> stayNodes = new ArrayList<ViewNode>();
       ArrayList<InetSocketAddress> stayServers =
           new ArrayList<InetSocketAddress>();
-      for (ViewNode current : nodes) {
+      for (ViewNode current : couchNodes) {
         if (newServerAddresses.contains(current.getSocketAddress())) {
           stayNodes.add(current);
           stayServers.add((InetSocketAddress) current.getSocketAddress());
@@ -176,7 +232,7 @@ public class CouchbaseConnection extends MemcachedConnection  implements
       mergedNodes.addAll(newNodes);
 
       // call update locator with new nodes list and vbucket config
-      nodes = mergedNodes;
+      couchNodes = mergedNodes;
 
       // schedule shutdown for the oddNodes
       nodesToShutdown.addAll(oddNodes);
@@ -188,90 +244,6 @@ public class CouchbaseConnection extends MemcachedConnection  implements
   }
 
   /**
-   * Add an operation to the given connection.
-   *
-   * @param key the key the operation is operating upon
-   * @param o the operation
-   */
-  public void addOperation(final String key, final Operation o) {
-    MemcachedNode placeIn = null;
-    MemcachedNode primary = locator.getPrimary(key);
-    if (primary.isActive() || failureMode == FailureMode.Retry) {
-      placeIn = primary;
-    } else if (failureMode == FailureMode.Cancel) {
-      o.cancel();
-    } else {
-      // Look for another node in sequence that is ready.
-      for (Iterator<MemcachedNode> i = locator.getSequence(key); placeIn == null
-          && i.hasNext();) {
-        MemcachedNode n = i.next();
-        if (n.isActive()) {
-          placeIn = n;
-        }
-      }
-      // If we didn't find an active node, queue it in the primary node
-      // and wait for it to come back online.
-      if (placeIn == null) {
-        placeIn = primary;
-        this.getLogger().warn(
-            "Could not redistribute "
-                + "to another node, retrying primary node for %s.", key);
-      }
-    }
-
-    assert o.isCancelled() || placeIn != null : "No node found for key " + key;
-    if (placeIn != null) {
-      // add the vbucketIndex to the operation
-      if (locator instanceof VBucketNodeLocator) {
-        VBucketNodeLocator vbucketLocator = (VBucketNodeLocator) locator;
-        short vbucketIndex = (short) vbucketLocator.getVBucketIndex(key);
-        if (o instanceof VBucketAware) {
-          VBucketAware vbucketAwareOp = (VBucketAware) o;
-          vbucketAwareOp.setVBucket(key, vbucketIndex);
-          if (!vbucketAwareOp.getNotMyVbucketNodes().isEmpty()) {
-            MemcachedNode alternative =
-                vbucketLocator.getAlternative(key,
-                    vbucketAwareOp.getNotMyVbucketNodes());
-            if (alternative != null) {
-              placeIn = alternative;
-            }
-          }
-        }
-      }
-      addOperation(placeIn, o);
-    } else {
-      assert o.isCancelled() : "No node found for " + key
-          + " (and not immediately cancelled)";
-    }
-  }
-
-  public void addOperations(final Map<MemcachedNode, Operation> ops) {
-
-    for (Map.Entry<MemcachedNode, Operation> me : ops.entrySet()) {
-      final MemcachedNode node = me.getKey();
-      Operation o = me.getValue();
-      // add the vbucketIndex to the operation
-      if (locator instanceof VBucketNodeLocator) {
-        if (o instanceof KeyedOperation && o instanceof VBucketAware) {
-          Collection<String> keys = ((KeyedOperation) o).getKeys();
-          VBucketNodeLocator vbucketLocator = (VBucketNodeLocator) locator;
-          for (String key : keys) {
-            short vbucketIndex = (short) vbucketLocator.getVBucketIndex(key);
-            VBucketAware vbucketAwareOp = (VBucketAware) o;
-            vbucketAwareOp.setVBucket(key, vbucketIndex);
-          }
-        }
-      }
-      o.setHandlingNode(node);
-      o.initialize();
-      node.addOp(o);
-      addedQueue.offer(node);
-    }
-    Selector s = selector.wakeup();
-    assert s == selector : "Wakeup returned the wrong selector.";
-  }
-
-  /**
    * Infinitely loop processing IO.
    */
   @Override
@@ -280,13 +252,9 @@ public class CouchbaseConnection extends MemcachedConnection  implements
       if (!reconfiguring) {
         try {
           handleIO();
-        } catch (IOException e) {
-          logRunException(e);
-        } catch (CancelledKeyException e) {
-          logRunException(e);
-        } catch (ClosedSelectorException e) {
-          logRunException(e);
         } catch (IllegalStateException e) {
+          logRunException(e);
+        } catch (Exception e) {
           logRunException(e);
         }
       }
