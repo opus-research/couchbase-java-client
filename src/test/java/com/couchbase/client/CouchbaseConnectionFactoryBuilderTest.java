a5428fd45b5e1e81490e7c5cca51fb92f05d5ae8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:00 +0000

    First commited as before_21805_rev2

--- src/test/java/com/couchbase/client/CouchbaseConnectionFactoryBuilderTest.java
+++ src/test/java/com/couchbase/client/CouchbaseConnectionFactoryBuilderTest.java
@@ -70,8 +70,6 @@ public class CouchbaseConnectionFactoryBuilderTest {
     long timeInterval = 600L;
     CouchbaseConnectionFactoryBuilder instance =
       new CouchbaseConnectionFactoryBuilder();
-    assertEquals("Did the test run with obsPollInterval=444",
-            444L, instance.getObsPollInterval());
     CouchbaseConnectionFactoryBuilder instanceResult =
       instance.setObsPollInterval(timeInterval);
     assertEquals("Failed to set observe poll interval.", 600L,
@@ -89,8 +87,6 @@ public class CouchbaseConnectionFactoryBuilderTest {
     int maxPoll = 40;
     CouchbaseConnectionFactoryBuilder instance =
       new CouchbaseConnectionFactoryBuilder();
-    assertEquals("Did the test run with ObsPollMax=99",
-            99L, instance.getObsPollMax());
     CouchbaseConnectionFactoryBuilder instanceResult
       = instance.setObsPollMax(maxPoll);
     assertEquals(instance, instanceResult);
