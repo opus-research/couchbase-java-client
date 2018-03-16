8bd13e69544a0ad75ecc0789a2e3e67a0cad31c7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:41:54 +0000

    First commited as after_70951_rev5

--- src/integration/java/com/couchbase/client/java/N1qlPreparedTest.java
+++ src/integration/java/com/couchbase/client/java/N1qlPreparedTest.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.Collections;
 import java.util.List;
 
-import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.query.AsyncN1qlQueryResult;
 import com.couchbase.client.java.query.AsyncN1qlQueryRow;
@@ -35,6 +34,7 @@ import com.couchbase.client.java.query.core.N1qlQueryExecutor;
 import com.couchbase.client.java.util.CouchbaseTestContext;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import rx.Observable;
 import rx.functions.Func1;
@@ -46,6 +46,7 @@ import rx.functions.Func6;
  * @author Simon Baslé
  * @since 2.1
  */
+@Ignore("travel-sample with indexes are required for these tests")
 public class N1qlPreparedTest {
 
     private static CouchbaseTestContext ctx;
@@ -56,54 +57,49 @@ public class N1qlPreparedTest {
     @BeforeClass
     public static void init() throws InterruptedException {
         ctx = CouchbaseTestContext.builder()
-                .adhoc(true)
-                .bucketQuota(100)
-                .bucketName("queryPrepared")
+                .adhoc(false)
+                .bucketName("travel-sample")
                 .build()
-                .ignoreIfNoN1ql()
-        .ensurePrimaryIndex();
+                .ignoreIfNoN1ql();
 
         executor = new N1qlQueryExecutor(ctx.cluster().core(), ctx.bucketName(), ctx.bucketPassword(), false);
-
-        ctx.bucket().upsert(JsonDocument.create("test1", JsonObject.create().put("item", "value")));
-        ctx.bucket().upsert(JsonDocument.create("test2", JsonObject.create().put("item", 123)));
     }
 
     @AfterClass
     public static void cleanup() {
-        ctx.destroyBucketAndDisconnect();
+        ctx.disconnect();
     }
 
     public static N1qlQueryResult query(N1qlQuery query) {
         return executor.execute(query)
                 .flatMap(new Func1<AsyncN1qlQueryResult, Observable<N1qlQueryResult>>() {
-                @Override
-                public Observable<N1qlQueryResult> call(AsyncN1qlQueryResult aqr) {
-                    final boolean parseSuccess = aqr.parseSuccess();
-                    final String requestId = aqr.requestId();
-                    final String clientContextId = aqr.clientContextId();
+                    @Override
+                    public Observable<N1qlQueryResult> call(AsyncN1qlQueryResult aqr) {
+                        final boolean parseSuccess = aqr.parseSuccess();
+                        final String requestId = aqr.requestId();
+                        final String clientContextId = aqr.clientContextId();
 
-                    return Observable.zip(aqr.rows().toList(),
-                        aqr.signature().singleOrDefault(JsonObject.empty()),
-                        aqr.info().singleOrDefault(N1qlMetrics.EMPTY_METRICS),
-                        aqr.errors().toList(),
-                        aqr.status(),
-                        aqr.finalSuccess().singleOrDefault(Boolean.FALSE),
-                        new Func6<List<AsyncN1qlQueryRow>, Object, N1qlMetrics, List<JsonObject>, String, Boolean, N1qlQueryResult>() {
-                            @Override
-                            public N1qlQueryResult call(List<AsyncN1qlQueryRow> rows, Object signature,
-                                                        N1qlMetrics info, List<JsonObject> errors, String finalStatus, Boolean finalSuccess) {
-                                return new DefaultN1qlQueryResult(rows, signature, info, errors, finalStatus, finalSuccess,
-                                    parseSuccess, requestId, clientContextId);
-                            }
-                        });
-                }
-            }).toBlocking().singleOrDefault(null);
+                        return Observable.zip(aqr.rows().toList(),
+                                aqr.signature().singleOrDefault(JsonObject.empty()),
+                                aqr.info().singleOrDefault(N1qlMetrics.EMPTY_METRICS),
+                                aqr.errors().toList(),
+                                aqr.status(),
+                                aqr.finalSuccess().singleOrDefault(Boolean.FALSE),
+                                new Func6<List<AsyncN1qlQueryRow>, Object, N1qlMetrics, List<JsonObject>, String, Boolean, N1qlQueryResult>() {
+                                    @Override
+                                    public N1qlQueryResult call(List<AsyncN1qlQueryRow> rows, Object signature,
+                                                                N1qlMetrics info, List<JsonObject> errors, String finalStatus, Boolean finalSuccess) {
+                                        return new DefaultN1qlQueryResult(rows, signature, info, errors, finalStatus, finalSuccess,
+                                                parseSuccess, requestId, clientContextId);
+                                    }
+                                });
+                    }
+                }).toBlocking().singleOrDefault(null);
     }
 
     @Test
     public void testPreparedWithEncodedPlanDisabledExecutor() {
-        N1qlQuery query = N1qlQuery.simple("SELECT * FROM `" + ctx.bucketName() + "`", N1qlParams.build().consistency(CONSISTENCY).adhoc(false));
+        N1qlQuery query = N1qlQuery.simple("SELECT * FROM `" + ctx.bucketName() + "` limit 2", N1qlParams.build().consistency(CONSISTENCY).adhoc(false));
         N1qlQueryResult result = query(query); //this uses the executor with encodedPlan forcefully disabled
         List<N1qlQueryRow> list = result.allRows();
         List<JsonObject> errors = result.errors();
@@ -116,5 +112,14 @@ public class N1qlPreparedTest {
         errors = result.errors();
         assertEquals("error during second iteration: " + errors, Collections.emptyList(), errors);
         assertEquals("result set too small during second iteration", 2, list.size());
+    }
+
+    @Test
+    public void testLongRunningPreparedQuery() {
+        int fetchSz = 20000;
+        N1qlQuery query = N1qlQuery.simple("select * from `"+ ctx.bucketName() +"` limit " + fetchSz, N1qlParams.build().adhoc(false));
+        N1qlQueryResult result = ctx.bucket().query(query);
+        System.out.println("Elapsed time:" + result.info().elapsedTime());
+        assertEquals("Result size incorrect", fetchSz, result.allRows().size());
     }
 }
