2df08b1dd84538f4b74afa010a5030e0cf66357b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:54 +0000

    First commited as after_41369_rev2

--- src/integration/java/com/couchbase/client/java/DesignDocumentTest.java
+++ src/integration/java/com/couchbase/client/java/DesignDocumentTest.java
@@ -7,6 +7,9 @@ import com.couchbase.client.java.util.ClusterDependentTest;
 import com.couchbase.client.java.view.*;
 import org.junit.Before;
 import org.junit.Test;
+import rx.Observable;
+import rx.Observer;
+import rx.functions.Func1;
 
 import java.util.Arrays;
 import java.util.List;
@@ -190,4 +193,34 @@ public class DesignDocumentTest extends ClusterDependentTest {
         manager.publishDesignDocument("pub2").toBlocking().single();
     }
 
+    @Test
+    public void shouldThrowErrorWhenQueryingMissingDesignDocument() {
+        ViewQuery queryTemplate = ViewQuery.from("test", "dummy");
+        queryTemplate.stale(Stale.FALSE);
+        Observable<ViewRow> viewRowObservable = bucket().query(queryTemplate).flatMap(new Func1<ViewResult, Observable<ViewRow>>() {
+            public Observable<ViewRow> call(ViewResult res) {
+                if (res.success()) {
+                    return res.rows();
+                } else {
+                    throw new DesignDocumentException(res.error().getString("error"));
+                }
+            }
+        });
+
+        viewRowObservable.subscribe(new Observer<ViewRow>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                assertEquals("not_found", throwable.getMessage());
+            }
+
+            @Override
+            public void onNext(ViewRow viewRow) {
+                throw new AssertionError("onNext() called on empty results");
+            }
+        });
+    }
 }
