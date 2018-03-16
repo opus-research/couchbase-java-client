d0ee8b038413969da28b7248c2b9acef383fa2d4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:41:12 +0000

    First commited as after_66034_rev1

--- src/main/java/com/couchbase/client/java/subdoc/AsyncMutateInBuilder.java
+++ src/main/java/com/couchbase/client/java/subdoc/AsyncMutateInBuilder.java
@@ -173,6 +173,47 @@ public class AsyncMutateInBuilder {
         }
     }
 
+    public Observable<DocumentFragment<Mutation>> execute(final PersistTo persistTo, final ReplicateTo replicateTo) {
+        Observable<DocumentFragment<Mutation>> mutationResult = execute();
+
+        if (persistTo == PersistTo.NONE && replicateTo == ReplicateTo.NONE) {
+            return mutationResult;
+        }
+
+        return mutationResult.flatMap(new Func1<DocumentFragment<Mutation>, Observable<DocumentFragment<Mutation>>>() {
+            @Override
+            public Observable<DocumentFragment<Mutation>> call(DocumentFragment<Mutation> fragment) {
+                return Observe.call(core, bucketName, docId, fragment.cas(), false, fragment.mutationToken(),
+                    persistTo.value(), replicateTo.value(),
+                    environment.observeIntervalDelay(), environment.retryStrategy())
+                    .map(new Func1<Boolean, DocumentFragment<Mutation>>() {
+                        @Override
+                        public DocumentFragment<Mutation> call(Boolean aBoolean) {
+                            return null;
+                        }
+                    }).onErrorResumeNext(new Func1<Throwable, Observable<DocumentFragment<Mutation>>>() {
+                        @Override
+                        public Observable<DocumentFragment<Mutation>> call(Throwable throwable) {
+                            return Observable.error(new DurabilityException(
+                                "Durability requirement failed: " + throwable.getMessage(),
+                                throwable));
+                        }
+                    });
+            }
+        });
+
+
+    }
+
+    public Observable<DocumentFragment<Mutation>> execute(PersistTo persistTo) {
+        return execute(persistTo, ReplicateTo.NONE);
+    }
+
+    public Observable<DocumentFragment<Mutation>> execute(ReplicateTo replicateTo) {
+        return execute(PersistTo.NONE, replicateTo);
+    }
+
+
     //==== DOCUMENT level modifiers ====
     /**
      * Change the expiry of the enclosing document as part of the mutation.
