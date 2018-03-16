db357677f592a7697950cde8ea8b75d4e9dd31f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:43 +0000

    First commited as after_16702_rev1

--- src/main/java/com/couchbase/client/CouchbaseConnectionFactoryBuilder.java
+++ src/main/java/com/couchbase/client/CouchbaseConnectionFactoryBuilder.java
@@ -65,6 +65,25 @@ public class CouchbaseConnectionFactoryBuilder extends ConnectionFactoryBuilder{
    *
    * @param baseList a list of URI's that will be used to connect to the cluster
    * @param bucketName the name of the bucket to connect to
+   * @param pwd the password for the bucket
+   * @return a CouchbaseConnectionFactory object
+   * @throws IOException
+   */
+  public CouchbaseConnectionFactory buildCouchbaseConnection(
+    final List<URI> baseList, final String bucketName, final String pwd)
+    throws IOException {
+    return buildCouchbaseConnection(baseList, bucketName, bucketName, pwd);
+  }
+
+  /**
+   * Get the CouchbaseConnectionFactory set up with the provided parameters.
+   * Note that a CouchbaseConnectionFactory requires the failure mode is set
+   * to retry, and the locator type is discovered dynamically based on the
+   * cluster you are connecting to. As a result, these values will be
+   * overridden upon calling this function.
+   *
+   * @param baseList a list of URI's that will be used to connect to the cluster
+   * @param bucketName the name of the bucket to connect to
    * @param usr the username for the bucket
    * @param pwd the password for the bucket
    * @return a CouchbaseConnectionFactory object
