bfb1222467d917c927c98de1b9cdc9991d3fd33e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:57 +0000

    First commited as before_61712_rev4

--- src/main/java/com/couchbase/client/java/transcoder/subdoc/JacksonFragmentTranscoder.java
+++ src/main/java/com/couchbase/client/java/transcoder/subdoc/JacksonFragmentTranscoder.java
@@ -23,21 +23,18 @@
 package com.couchbase.client.java.transcoder.subdoc;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import com.couchbase.client.core.annotations.InterfaceAudience;
 import com.couchbase.client.core.annotations.InterfaceStability;
 import com.couchbase.client.deps.com.fasterxml.jackson.core.JsonProcessingException;
 import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectMapper;
 import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
-import com.couchbase.client.deps.io.netty.buffer.ByteBufOutputStream;
 import com.couchbase.client.deps.io.netty.buffer.Unpooled;
 import com.couchbase.client.java.error.TranscodingException;
-import com.couchbase.client.java.subdoc.MultiValue;
 import com.couchbase.client.java.transcoder.TranscoderUtils;
 
 /**
- * A Jackson-based implementation of a {@link FragmentTranscoder}, based on {@link AbstractFragmentTranscoder}.
+ * A Jackson-based implementation of a {@link FragmentTranscoder}.
  *
  * Care should be taken to not use Jackson specific annotations if you want to be able to
  * easily swap this for another SubdocumentTranscoder implementation at a later time.
@@ -47,12 +44,17 @@ import com.couchbase.client.java.transcoder.TranscoderUtils;
  */
 @InterfaceStability.Experimental
 @InterfaceAudience.Private
-public class JacksonFragmentTranscoder extends AbstractFragmentTranscoder {
+public class JacksonFragmentTranscoder implements FragmentTranscoder {
 
     private final ObjectMapper mapper;
 
     public JacksonFragmentTranscoder(ObjectMapper mapper) {
         this.mapper = mapper;
+    }
+
+    @Override
+    public <T> T decode(ByteBuf encoded, Class<? extends T> clazz) throws TranscodingException {
+        return this.decodeWithMessage(encoded, clazz, null);
     }
 
     @Override
@@ -70,31 +72,16 @@ public class JacksonFragmentTranscoder extends AbstractFragmentTranscoder {
     }
 
     @Override
-    protected <T> ByteBuf doEncodeSingle(T value, String transcodingErrorMessage) throws TranscodingException {
+    public <T> ByteBuf encode(T value) throws TranscodingException {
+        return encodeWithMessage(value, null);
+    }
+
+    @Override
+    public <T> ByteBuf encodeWithMessage(T value, String transcodingErrorMessage) throws TranscodingException {
         try {
             return Unpooled.wrappedBuffer(mapper.writeValueAsBytes(value));
         } catch (JsonProcessingException e) {
             throw new TranscodingException(transcodingErrorMessage, e);
         }
-    }
-
-    @Override
-    protected ByteBuf doEncodeMulti(MultiValue<?> multiValue, String transcodingErrorMessage) throws TranscodingException {
-        //initial capacity is very roughly and arbitrarily initialized (4 bytes on average per value)
-        final ByteBufOutputStream out = new ByteBufOutputStream(Unpooled.buffer(4 * multiValue.size()));
-        //Note this OutputStream implementation doesn't implement flush() nor close(), so they are left out.
-        try {
-            for (Iterator<?> iterator = multiValue.iterator(); iterator.hasNext(); ) {
-                Object o = iterator.next();
-                mapper.writeValue(out, o);
-                if (iterator.hasNext()) {
-                   out.writeBytes(",");
-                }
-            }
-            return out.buffer();
-        } catch (IOException e) {
-            throw new TranscodingException(transcodingErrorMessage, e);
-        }
-        //changing the OutputStream concrete implementation would probably require to close() in a finally block
     }
 }
