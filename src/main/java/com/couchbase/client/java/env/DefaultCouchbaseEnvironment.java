7831f5495e72b19672604f29d4d24922c56e3828
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:15 +0000

    First commited as before_42415_rev3

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -21,6 +21,9 @@
  */
 package com.couchbase.client.java.env;
 
+import com.couchbase.client.core.ClusterFacade;
+import com.couchbase.client.core.CouchbaseCore;
+import com.couchbase.client.core.env.CoreEnvironment;
 import com.couchbase.client.core.env.DefaultCoreEnvironment;
 import com.couchbase.client.core.logging.CouchbaseLogger;
 import com.couchbase.client.core.logging.CouchbaseLoggerFactory;
@@ -67,8 +70,7 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
     private final long connectTimeout;
     private final long disconnectTimeout;
 
-    public static String SDK_PACKAGE_NAME_AND_VERSION = "couchbase-java-client";
-    public static String SDK_USER_AGENT = SDK_PACKAGE_NAME_AND_VERSION;
+    public static String PACKAGE_NAME_AND_VERSION = "couchbase-java-client";
 
     private static final String VERSION_PROPERTIES = "com.couchbase.client.java.properties";
 
@@ -96,11 +98,11 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
             } catch (Exception e) {
                 LOGGER.info("Could not retrieve version properties, defaulting.", e);
             }
-            SDK_PACKAGE_NAME_AND_VERSION = String.format("couchbase-java-client/%s (git: %s)",
+            PACKAGE_NAME_AND_VERSION = String.format("couchbase-java-client/%s (git: %s)",
                 version == null ? "unknown" : version, gitVersion == null ? "unknown" : gitVersion);
 
-            SDK_USER_AGENT = String.format("%s (%s/%s %s; %s %s)",
-                SDK_PACKAGE_NAME_AND_VERSION,
+            USER_AGENT = String.format("%s (%s/%s %s; %s %s)",
+                PACKAGE_NAME_AND_VERSION,
                 System.getProperty("os.name"),
                 System.getProperty("os.version"),
                 System.getProperty("os.arch"),
@@ -149,8 +151,8 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         private long connectTimeout = CONNECT_TIMEOUT;
         private long disconnectTimeout = DISCONNECT_TIMEOUT;
 
-        private String userAgent = SDK_USER_AGENT;
-        private String packageNameAndVersion = SDK_PACKAGE_NAME_AND_VERSION;
+        private String userAgent = USER_AGENT;
+        private String packageNameAndVersion = PACKAGE_NAME_AND_VERSION;
 
         @Override
         public long managementTimeout() {
