391db91c47f5aa08acd81684e5399d8f464efc6d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:41 +0000

    First commited as after_31214_rev5

--- src/test/java/com/couchbase/client/BucketTool.java
+++ src/test/java/com/couchbase/client/BucketTool.java
@@ -159,12 +159,12 @@ public class BucketTool extends SpyObject {
         Map<String, String> serverStats = server.getValue();
         if (!serverStats.containsKey("ep_degraded_mode")) {
           warmup = true;
-          Thread.sleep(1000);
+          Thread.sleep(SLEEP_TIME);
           break;
         }
         if (!serverStats.get("ep_degraded_mode").equals("0")) {
           warmup = true;
-          Thread.sleep(1000);
+          Thread.sleep(SLEEP_TIME);
           break;
         }
       }
