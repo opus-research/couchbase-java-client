f57d919ee5fdb7cc6afc2b4212560e389c7a6bab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:42:40 +0000

    First commited as before_77516_rev4

--- src/main/java/com/couchbase/client/java/subdoc/AsyncMutateInBuilder.java
+++ src/main/java/com/couchbase/client/java/subdoc/AsyncMutateInBuilder.java
@@ -27,7 +27,6 @@ import com.couchbase.client.core.annotations.InterfaceAudience;
 import com.couchbase.client.core.annotations.InterfaceStability;
 import com.couchbase.client.core.logging.CouchbaseLogger;
 import com.couchbase.client.core.logging.CouchbaseLoggerFactory;
-import com.couchbase.client.core.message.CouchbaseRequest;
 import com.couchbase.client.core.message.ResponseStatus;
 import com.couchbase.client.core.message.kv.MutationToken;
 import com.couchbase.client.core.message.kv.subdoc.multi.MultiMutationResponse;
@@ -71,7 +70,6 @@ import com.couchbase.client.java.error.subdoc.PathNotFoundException;
 import com.couchbase.client.java.error.subdoc.SubDocumentException;
 import com.couchbase.client.java.transcoder.subdoc.FragmentTranscoder;
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.functions.Func2;
@@ -1321,9 +1319,9 @@ public class AsyncMutateInBuilder {
     private Observable<DocumentFragment<Mutation>> doSingleMutate(final MutationSpec spec,
             final Func2<MutationSpec, ByteBuf, ? extends AbstractSubdocMutationRequest> requestFactory,
             final Func3<ResponseStatus, String, String, Object> responseStatusDocIdAndPathToValueEvaluator) {
-        return deferAndWatch(new Func1<Subscriber, Observable<SimpleSubdocResponse>>() {
+        return deferAndWatch(new Func0<Observable<SimpleSubdocResponse>>() {
             @Override
-            public Observable<SimpleSubdocResponse> call(Subscriber s) {
+            public Observable<SimpleSubdocResponse> call() {
                 ByteBuf buf;
                 try {
                     buf = subdocumentTranscoder.encodeWithMessage(spec.fragment(),
@@ -1333,9 +1331,7 @@ public class AsyncMutateInBuilder {
                     return Observable.error(e);
                 }
 
-                AbstractSubdocMutationRequest request = requestFactory.call(spec, buf);
-                request.subscriber(s);
-                return core.send(request);
+                return core.send(requestFactory.call(spec, buf));
             }
         }).map(new Func1<SimpleSubdocResponse, DocumentFragment<Mutation>>() {
             @Override
@@ -1364,11 +1360,10 @@ public class AsyncMutateInBuilder {
 
     private Observable<DocumentFragment<Mutation>> removeIn(final MutationSpec spec) {
         return deferAndWatch(
-                new Func1<Subscriber, Observable<SimpleSubdocResponse>>() {
+                new Func0<Observable<SimpleSubdocResponse>>() {
                     @Override
-                    public Observable<SimpleSubdocResponse> call(Subscriber s) {
+                    public Observable<SimpleSubdocResponse> call() {
                         SubDeleteRequest request = new SubDeleteRequest(docId, spec.path(), bucketName, expiry, cas);
-                        request.subscriber(s);
                         request.xattr(spec.xattr());
                         return core.send(request);
                     }
@@ -1413,13 +1408,13 @@ public class AsyncMutateInBuilder {
 
         final long delta = fragment.longValue();
 
-        return deferAndWatch(new Func1<Subscriber, Observable<SimpleSubdocResponse>>() {
+        return deferAndWatch(
+                new Func0<Observable<SimpleSubdocResponse>>() {
                     @Override
-                    public Observable<SimpleSubdocResponse> call(Subscriber s) {
+                    public Observable<SimpleSubdocResponse> call() {
                         SubCounterRequest request = new SubCounterRequest(docId, spec.path(), delta, bucketName, expiry, cas);
                         request.createIntermediaryPath(spec.createParents());
                         request.xattr(spec.xattr());
-                        request.subscriber(s);
                         return core.send(request);
                     }
                 })
