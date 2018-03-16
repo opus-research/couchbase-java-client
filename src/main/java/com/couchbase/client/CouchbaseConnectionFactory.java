43c598cdb2001f9163d65f63262b847261024ae9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:37 +0000

    First commited as before_14485_rev3

--- src/main/java/com/couchbase/client/CouchbaseConnectionFactory.java
+++ src/main/java/com/couchbase/client/CouchbaseConnectionFactory.java
@@ -111,7 +111,7 @@ public class CouchbaseConnectionFactory extends BinaryConnectionFactory {
     }
 
     if (config.getConfigType() == ConfigType.MEMCACHE) {
-      return new KetamaNodeLocator(nodes, DefaultHashAlgorithm.KETAMA_HASH);
+      return new KetamaNodeLocator(nodes, getHashAlg());
     } else if (config.getConfigType() == ConfigType.COUCHBASE) {
       return new VBucketNodeLocator(nodes, getVBucketConfig());
     } else {
