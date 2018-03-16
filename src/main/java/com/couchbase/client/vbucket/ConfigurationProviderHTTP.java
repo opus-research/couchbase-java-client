b9bc7bcca67ccfc613b63b9b644e8c2a031601a7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:49 +0000

    First commited as after_32425_rev1

--- src/main/java/com/couchbase/client/vbucket/ConfigurationProviderHTTP.java
+++ src/main/java/com/couchbase/client/vbucket/ConfigurationProviderHTTP.java
@@ -476,4 +476,14 @@ public class ConfigurationProviderHTTP extends SpyObject implements
     baseList = newList;
   }
 
+  @Override
+  public void updateBucket(String config) {
+    try {
+      updateBucket(getBucket(), configurationParser.parseBucket(config));
+    } catch (Exception ex) {
+      getLogger().warn("Got new config to update, but could not decode it. "
+        + "Staying with old one.", ex);
+    }
+  }
+
 }
