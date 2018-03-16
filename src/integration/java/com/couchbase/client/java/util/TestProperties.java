1f328c7d73452b8c9f7a3ee00283688fbdc75c12
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:07 +0000

    First commited as before_42074_rev2

--- src/integration/java/com/couchbase/client/java/util/TestProperties.java
+++ src/integration/java/com/couchbase/client/java/util/TestProperties.java
@@ -34,7 +34,6 @@ public class TestProperties {
   private static String password;
   private static String adminName;
   private static String adminPassword;
-  private static int thinkTime;
 
   /**
    * Initialize static the properties.
@@ -45,7 +44,6 @@ public class TestProperties {
     password = System.getProperty("password", "");
     adminName = System.getProperty("adminName", "Administrator");
     adminPassword = System.getProperty("adminPassword", "password");
-    thinkTime = Integer.parseInt(System.getProperty("thinkTime", "5000"));
   }
 
   /**
@@ -82,8 +80,4 @@ public class TestProperties {
   public static String adminPassword() {
       return adminPassword;
   }
-
-    public static int thinkTime() {
-        return thinkTime;
-    }
 }
