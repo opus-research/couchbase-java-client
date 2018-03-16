fc568d965f8265d51a2f0ec453a904d80b4c65f4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:24 +0000

    First commited as before_29467_rev1

--- src/test/java/com/couchbase/client/vbucket/config/ConfigurationParserMock.java
+++ src/test/java/com/couchbase/client/vbucket/config/ConfigurationParserMock.java
@@ -26,7 +26,6 @@ package com.couchbase.client.vbucket.config;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -47,7 +46,7 @@ public class ConfigurationParserMock implements ConfigurationParser {
   private String poolStreamingUri = "/poolsStreaming/default";
   private String bucketName = "Administrator";
   private DefaultConfig vbuckets = new DefaultConfig(
-    DefaultHashAlgorithm.NATIVE_HASH, 1, 1, 1, new ArrayList<String>(), null, null);
+    DefaultHashAlgorithm.NATIVE_HASH, 1, 1, 1, null, null, null);
   private String bucketsUri = "/pools/default/buckets";
   private String bucketStreamingUri =
       "/pools/default/bucketsStreaming/Administrator";
