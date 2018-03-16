7102e1f2203497039ac3cd6b09d959bbc9488943
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:08 +0000

    First commited as before_45983_rev2

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -59,7 +59,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
     private static final long KV_TIMEOUT = 2500;
     private static final long CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
     private static final long DISCONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
-    private static final boolean DNS_SRV_ENABLED = false;
 
     private final long managementTimeout;
     private final long queryTimeout;
@@ -67,7 +66,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
     private final long kvTimeout;
     private final long connectTimeout;
     private final long disconnectTimeout;
-    private final boolean dnsSrvEnabled;
 
     public static String SDK_PACKAGE_NAME_AND_VERSION = "couchbase-java-client";
 
@@ -123,7 +121,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         kvTimeout = longPropertyOr("kvTimeout", builder.kvTimeout());
         connectTimeout = longPropertyOr("connectTimeout", builder.connectTimeout());
         disconnectTimeout = longPropertyOr("disconnectTimeout", builder.disconnectTimeout());
-        dnsSrvEnabled = booleanPropertyOr("dnsSrvEnabled", builder.dnsSrvEnabled());
 
         if (queryTimeout > maxRequestLifetime()) {
             LOGGER.warn("The configured query timeout is greater than the maximum request lifetime. " +
@@ -169,7 +166,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         private long kvTimeout = KV_TIMEOUT;
         private long connectTimeout = CONNECT_TIMEOUT;
         private long disconnectTimeout = DISCONNECT_TIMEOUT;
-        private boolean dnsSrvEnabled = DNS_SRV_ENABLED;
 
         private String userAgent = USER_AGENT; //this is from Core
         private String packageNameAndVersion = SDK_PACKAGE_NAME_AND_VERSION;
@@ -417,16 +413,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
-        public boolean dnsSrvEnabled() {
-            return dnsSrvEnabled;
-        }
-
-        public Builder dnsSrvEnabled(boolean dnsSrvEnabled) {
-            this.dnsSrvEnabled = dnsSrvEnabled;
-            return this;
-        }
-
-        @Override
         public DefaultCouchbaseEnvironment build() {
             return new DefaultCouchbaseEnvironment(this);
         }
@@ -460,10 +446,5 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
     @Override
     public long disconnectTimeout() {
         return disconnectTimeout;
-    }
-
-    @Override
-    public boolean dnsSrvEnabled() {
-        return dnsSrvEnabled;
     }
 }
