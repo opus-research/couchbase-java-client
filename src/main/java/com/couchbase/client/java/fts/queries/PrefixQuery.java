3660ff602c051a19aba23c213c0a891a3bfe4808
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:40:10 +0000

    First commited as after_62648_rev12

---
+++ src/main/java/com/couchbase/client/java/fts/queries/PrefixQuery.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.java.fts.queries;
+
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.fts.SearchParams;
+import com.couchbase.client.java.fts.SearchQuery;
+
+public class PrefixQuery extends SearchQuery {
+
+    private final String prefix;
+    private String field;
+
+    public PrefixQuery(String prefix) {
+        super();
+        this.prefix = prefix;
+    }
+
+    public PrefixQuery field(String field) {
+        this.field = field;
+        return this;
+    }
+
+    @Override
+    public PrefixQuery boost(double boost) {
+        super.boost(boost);
+        return this;
+    }
+
+    @Override
+    protected void injectParams(JsonObject input) {
+        input.put("prefix", this.prefix);
+        if (field != null) {
+            input.put("field", field);
+        }
+    }
+}
---
+++ src/test/java/com/couchbase/client/java/fts/queries/DateRangeQueryTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.java.fts.queries;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.fts.SearchParams;
+import com.couchbase.client.java.fts.SearchQuery;
+import org.junit.Test;
+
+public class DateRangeQueryTest {
+
+    @Test
+    public void shouldFailIfNoBounds() {
+        DateRangeQuery query = SearchQuery.dateRange();
+        catchException(query).export();
+
+        assertTrue(caughtException() instanceof NullPointerException);
+        assertTrue(caughtException().getMessage().contains("start or end"));
+    }
+
+    @Test
+    public void shouldAcceptEndOnly() {
+        DateRangeQuery query = SearchQuery.dateRange().end("theEnd");
+        JsonObject expected = JsonObject.create()
+            .put("query", JsonObject.create()
+                .put("end", "theEnd")
+                .put("inclusive_end", false));
+        assertEquals(expected, query.export());
+    }
+
+    @Test
+    public void shouldAcceptStartOnly() {
+        DateRangeQuery query = SearchQuery.dateRange().start("theStart");
+        JsonObject expected = JsonObject.create()
+            .put("query", JsonObject.create()
+                .put("start", "theStart")
+                .put("inclusive_start", true));
+        assertEquals(expected, query.export());
+    }
+
+    @Test
+    public void shouldExplicitlySetDefaultsForInclusiveStartAndEnd() {
+        SearchParams params = SearchParams.build().explain();
+        DateRangeQuery query = SearchQuery.dateRange()
+            .boost(1.5)
+            .field("field")
+            .start("a")
+            .end("b")
+            .dateTimeParser("parser");
+
+        JsonObject expected = JsonObject.create()
+            .put("query", JsonObject.create()
+                .put("start", "a")
+                .put("inclusive_start", true)
+                .put("end", "b")
+                .put("inclusive_end", false)
+                .put("datetime_parser", "parser")
+                .put("boost", 1.5)
+                .put("field", "field"))
+            .put("explain", true);
+        assertEquals(expected, query.export(params));
+    }
+
+    @Test
+    public void shouldExportDateRangeQueryWithAllOptions() {
+        SearchParams params = SearchParams.build().explain();
+        DateRangeQuery query = SearchQuery.dateRange()
+            .boost(1.5)
+            .field("field")
+            .start("a", false)
+            .end("b", true)
+            .dateTimeParser("parser");
+
+        JsonObject expected = JsonObject.create()
+            .put("query", JsonObject.create()
+                .put("start", "a")
+                .put("inclusive_start", false)
+                .put("end", "b")
+                .put("inclusive_end", true)
+                .put("datetime_parser", "parser")
+                .put("boost", 1.5)
+                .put("field", "field"))
+            .put("explain", true);
+        assertEquals(expected, query.export(params));
+    }
+
+}
