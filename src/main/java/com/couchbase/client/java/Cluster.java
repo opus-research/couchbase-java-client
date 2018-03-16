8ed8a9b1d5e9e084fcc939ddcc4c0d13a8932733
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:10 +0000

    First commited as before_36779_rev1

--- src/main/java/com/couchbase/client/java/Cluster.java
+++
@@ -1,9 +0,0 @@
-package com.couchbase.client.java;
-
-import rx.Observable;
-
-public interface Cluster {
-
-  Observable<Bucket> openBucket(String name);
-  Observable<Bucket> openBucket(String name, String password);
-}
--- src/main/java/com/couchbase/client/java/document/json/JsonValue.java
+++
@@ -1,8 +0,0 @@
-package com.couchbase.client.java.document.json;
-
-/**
- * Marker interface for values which can be stored inside a
- * {@link com.couchbase.client.java.document.JsonDocument}.
- */
-public interface JsonValue {
-}
