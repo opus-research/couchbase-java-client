a6b73ff9298b1a21c6a7f9b36586e2405d8439ce
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:45 +0000

    First commited as before_31710_rev1

--- src/main/java/com/couchbase/client/CouchbaseClient.java
+++ src/main/java/com/couchbase/client/CouchbaseClient.java
@@ -249,6 +249,8 @@ public class CouchbaseClient extends MemcachedClient
   public CouchbaseClient(CouchbaseConnectionFactory cf)
     throws IOException {
     super(cf, AddrUtil.getAddresses(cf.getVBucketConfig().getServers()));
+    getLogger().info(cf.toString());
+
     cbConnFactory = cf;
 
     if(cf.getVBucketConfig().getConfigType() == ConfigType.COUCHBASE) {
@@ -1229,8 +1231,9 @@ public class CouchbaseClient extends MemcachedClient
     final ReplicateTo rep, final String prefix, final boolean delete) {
 
     final CountDownLatch latch = new CountDownLatch(1);
+
     final ObserveFuture<Boolean> observeFuture = new ObserveFuture<Boolean>(
-      key, latch, operationTimeout, executorService);
+      key, latch, cbConnFactory.getObsTimeout(), executorService);
 
     original.addListener(new OperationCompletionListener() {
       @Override
@@ -1321,7 +1324,13 @@ public class CouchbaseClient extends MemcachedClient
     try {
       OperationFuture<CASResponse> casOp = asyncCas(key, cas, exp, value, req,
         rep);
-      casr = casOp.get(operationTimeout, TimeUnit.MILLISECONDS);
+
+      long timeout = cbConnFactory.getObsTimeout();
+      if (req == PersistTo.ZERO && rep == ReplicateTo.ZERO) {
+        timeout = operationTimeout;
+      }
+
+      casr = casOp.get(timeout, TimeUnit.MILLISECONDS);
       return casr;
     } catch (InterruptedException e) {
       throw new RuntimeException("Interrupted waiting for value", e);
@@ -1404,7 +1413,7 @@ public class CouchbaseClient extends MemcachedClient
 
     final CountDownLatch latch = new CountDownLatch(1);
     final ObserveFuture<CASResponse> observeFuture =
-      new ObserveFuture<CASResponse>(key, latch, operationTimeout,
+      new ObserveFuture<CASResponse>(key, latch, cbConnFactory.getObsTimeout(),
         executorService);
 
     casOp.addListener(new OperationCompletionListener() {
