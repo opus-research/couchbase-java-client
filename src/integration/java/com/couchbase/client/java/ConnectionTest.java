e7792267e22854022ff711c63a4a1377dc61831e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:09 +0000

    First commited as before_42121_rev3

--- src/integration/java/com/couchbase/client/java/ConnectionTest.java
+++ src/integration/java/com/couchbase/client/java/ConnectionTest.java
@@ -21,8 +21,7 @@
  */
 package com.couchbase.client.java;
 
-import com.couchbase.client.java.error.BucketDoesNotExistException;
-import com.couchbase.client.java.error.InvalidPasswordException;
+import com.couchbase.client.core.config.ConfigurationException;
 import com.couchbase.client.java.util.TestProperties;
 import org.junit.Test;
 
@@ -46,13 +45,13 @@ public class ConnectionTest {
         cluster.openBucket("");
     }
 
-    @Test(expected = BucketDoesNotExistException.class)
+    @Test(expected = ConfigurationException.class)
     public void shouldThrowConfigurationExceptionForWrongBucketName() {
         Cluster cluster = CouchbaseCluster.create(TestProperties.seedNode());
         cluster.openBucket("someWrongBucketName");
     }
 
-    @Test(expected = InvalidPasswordException.class)
+    @Test(expected = ConfigurationException.class)
     public void shouldThrowConfigurationExceptionForWrongBucketPassword() {
         Cluster cluster = CouchbaseCluster.create(TestProperties.seedNode());
         cluster.openBucket(TestProperties.bucket(), "completelyWrongPassword");
