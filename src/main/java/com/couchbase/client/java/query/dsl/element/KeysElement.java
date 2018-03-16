e2046eafb05684166a7cbb397803e8a4149e0a61
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:39 +0000

    First commited as after_49606_rev1

--- src/main/java/com/couchbase/client/java/query/dsl/element/KeysElement.java
+++ src/main/java/com/couchbase/client/java/query/dsl/element/KeysElement.java
@@ -11,12 +11,30 @@ public class KeysElement implements Element {
 
     private final Expression expression;
 
-    public KeysElement(Expression expression) {
+    private final ClauseType clauseType;
+
+    public KeysElement(ClauseType clauseType, Expression expression) {
+        this.clauseType = clauseType;
         this.expression = expression;
     }
 
     @Override
     public String export() {
-        return "KEYS " + expression.toString();
+        return clauseType.n1ql + expression.toString();
+    }
+
+    public static enum ClauseType {
+
+        JOIN_ON("ON KEYS "),
+        USE_KEYSPACE("USE KEYS "),
+        JOIN_ON_PRIMARY("ON PRIMARY KEYS "),
+        USE_KEYSPACE_PRIMARY("USE PRIMARY KEYS ");
+
+        private final String n1ql;
+
+        ClauseType(String n1ql) {
+            this.n1ql = n1ql;
+        }
+
     }
 }
--- src/main/java/com/couchbase/client/java/query/dsl/path/KeysPath.java
+++ src/main/java/com/couchbase/client/java/query/dsl/path/KeysPath.java
@@ -9,10 +9,21 @@ import com.couchbase.client.java.query.dsl.Expression;
  * @author Michael Nitschinger
  */
 public interface KeysPath extends LetPath {
-
+    /** the on-key clause of a join clause */
     LetPath keys(Expression expression);
 
-    LetPath keys(String key);
+    /** the on-key clause of a join clause */
+    LetPath keys(String... keys);
 
+    /** the on-key clause of a join clause */
     LetPath keys(JsonArray keys);
+
+    /** use the primary keyspace (doc id) in a join clause) */
+    LetPath useKeys(Expression expression);
+
+    /** use the primary keyspace (doc id) in a join clause) */
+    LetPath useKeys(String... keys);
+
+    /** use the primary keyspace (doc id) in a join clause) */
+    LetPath useKeys(JsonArray keys);
 }
