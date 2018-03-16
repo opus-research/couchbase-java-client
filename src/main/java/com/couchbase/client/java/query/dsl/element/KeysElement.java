1c6a4b6f552a85de71b0831f59c91fdad16765ce
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:40 +0000

    First commited as before_49606_rev3

--- src/main/java/com/couchbase/client/java/query/dsl/element/KeysElement.java
+++ src/main/java/com/couchbase/client/java/query/dsl/element/KeysElement.java
@@ -11,30 +11,12 @@ public class KeysElement implements Element {
 
     private final Expression expression;
 
-    private final ClauseType clauseType;
-
-    public KeysElement(ClauseType clauseType, Expression expression) {
-        this.clauseType = clauseType;
+    public KeysElement(Expression expression) {
         this.expression = expression;
     }
 
     @Override
     public String export() {
-        return clauseType.n1ql + expression.toString();
-    }
-
-    public static enum ClauseType {
-
-        JOIN_ON("ON KEYS "),
-        USE_KEYSPACE("USE KEYS "),
-        JOIN_ON_PRIMARY("ON PRIMARY KEYS "),
-        USE_KEYSPACE_PRIMARY("USE PRIMARY KEYS ");
-
-        private final String n1ql;
-
-        ClauseType(String n1ql) {
-            this.n1ql = n1ql;
-        }
-
+        return "KEYS " + expression.toString();
     }
 }
