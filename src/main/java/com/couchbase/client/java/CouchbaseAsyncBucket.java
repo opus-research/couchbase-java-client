c8d12334f7c955d308395eb4451416e7c3299580
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:42:15 +0000

    First commited as before_73513_rev5

--- src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
+++ src/main/java/com/couchbase/client/java/CouchbaseAsyncBucket.java
@@ -57,9 +57,6 @@ import com.couchbase.client.core.message.search.SearchQueryResponse;
 import com.couchbase.client.core.message.view.ViewQueryRequest;
 import com.couchbase.client.core.message.view.ViewQueryResponse;
 import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
-import com.couchbase.client.java.analytics.AnalyticsQuery;
-import com.couchbase.client.java.analytics.AnalyticsQueryExecutor;
-import com.couchbase.client.java.analytics.AsyncAnalyticsQueryResult;
 import com.couchbase.client.java.bucket.AsyncBucketManager;
 import com.couchbase.client.java.bucket.DefaultAsyncBucketManager;
 import com.couchbase.client.java.bucket.ReplicaReader;
@@ -155,7 +152,6 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
     private final CouchbaseEnvironment environment;
     /** the bucket's {@link N1qlQueryExecutor}. Prefer using {@link #n1qlQueryExecutor()} since it allows mocking and testing */
     private final N1qlQueryExecutor n1qlQueryExecutor;
-    private final AnalyticsQueryExecutor analyticsQueryExecutor;
 
     private volatile boolean closed;
 
@@ -190,7 +186,6 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
 
         boolean n1qlPreparedEncodedPlanEnabled = "true".equalsIgnoreCase(System.getProperty(N1qlQueryExecutor.ENCODED_PLAN_ENABLED_PROPERTY, "true")); //active by default
         n1qlQueryExecutor = new N1qlQueryExecutor(core, bucket, password, n1qlPreparedEncodedPlanEnabled);
-        analyticsQueryExecutor = new AnalyticsQueryExecutor(core, bucket, password);
     }
 
     @Override
@@ -866,15 +861,6 @@ public class CouchbaseAsyncBucket implements AsyncBucket {
             query.params().serverSideTimeout(environment().queryTimeout(), TimeUnit.MILLISECONDS);
         }
         return n1qlQueryExecutor.execute(query);
-    }
-
-    public Observable<AsyncAnalyticsQueryResult> query(final AnalyticsQuery query) {
-      /* TODO once exposed on the server
-        if (!query.params().hasServerSideTimeout()) {
-            query.params().serverSideTimeout(environment().queryTimeout(), TimeUnit.MILLISECONDS);
-        }
-        */
-        return analyticsQueryExecutor.execute(query);
     }
 
     @Override
