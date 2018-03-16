2ab3d550b50bdae8f0c4131ba108c7b829dc856c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:10 +0000

    First commited as before_57844_rev3

--- src/test/java/com/couchbase/client/java/query/N1qlParamsTest.java
+++ src/test/java/com/couchbase/client/java/query/N1qlParamsTest.java
@@ -21,7 +21,6 @@
  */
 package com.couchbase.client.java.query;
 
-import com.couchbase.client.core.message.kv.MutationToken;
 import com.couchbase.client.java.SerializationHelper;
 import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.query.consistency.ScanConsistency;
@@ -234,65 +233,5 @@ public class N1qlParamsTest {
         source.injectParams(actual);
 
         assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldInjectMutationTokenOnAtPlus() throws Exception {
-        MutationToken token = new MutationToken(1, 2345, 567);
-        N1qlParams source = N1qlParams.build()
-            .consistency(ScanConsistency.AT_PLUS)
-            .scanTokens(token);
-
-        JsonObject actual = JsonObject.empty();
-        source.injectParams(actual);
-
-        JsonObject expected = JsonObject.create()
-            .put("scan_consistency", "at_plus")
-            .put("scan_vector", JsonObject.create().put("1",
-                JsonObject.create().put("value", 567L).put("guard", "2345")));
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldInjectMutationTokensOnAtPlus() throws Exception {
-        MutationToken token1 = new MutationToken(1, 2345, 567);
-        MutationToken token2 = new MutationToken(5, 2, 3);
-        N1qlParams source = N1qlParams.build()
-            .consistency(ScanConsistency.AT_PLUS)
-            .scanTokens(token1, token2);
-
-        JsonObject actual = JsonObject.empty();
-        source.injectParams(actual);
-
-        JsonObject expected = JsonObject.create()
-            .put("scan_consistency", "at_plus")
-            .put("scan_vector", JsonObject.create()
-                .put("1", JsonObject.create().put("value", 567L).put("guard", "2345"))
-                .put("5", JsonObject.create().put("value", 3L).put("guard", "2"))
-            );
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldIgnoreTokenIfNotAtPlus() throws Exception {
-        MutationToken token = new MutationToken(1, 2345, 567);
-        N1qlParams source = N1qlParams.build()
-            .consistency(ScanConsistency.REQUEST_PLUS)
-            .scanTokens(token);
-
-        JsonObject actual = JsonObject.empty();
-        source.injectParams(actual);
-
-        JsonObject expected = JsonObject.create().put("scan_consistency", "request_plus");
-        assertEquals(expected, actual);
-    }
-
-    @Test(expected =  IllegalArgumentException.class)
-    public void shouldFailIfNoTokensOnAtPlus() throws Exception {
-        N1qlParams source = N1qlParams.build()
-            .consistency(ScanConsistency.AT_PLUS);
-
-        JsonObject actual = JsonObject.empty();
-        source.injectParams(actual);
     }
 }
