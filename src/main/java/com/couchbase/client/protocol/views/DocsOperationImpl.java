f6bd7838e6678b9927b557a0fc71e3bc4b3654d3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:33:45 +0000

    First commited as before_18094_rev2

--- src/main/java/com/couchbase/client/protocol/views/DocsOperationImpl.java
+++ src/main/java/com/couchbase/client/protocol/views/DocsOperationImpl.java
@@ -73,24 +73,4 @@ public class DocsOperationImpl extends ViewOperationImpl {
     }
     return new ViewResponseWithDocs(rows, errors);
   }
-
-  @Override
-  protected ViewResponseWithDocs parseError(String json, int errorcode)
-      throws ParseException {
-    final Collection<ViewRow> rows = new LinkedList<ViewRow>();
-    final Collection<RowError> errors = new LinkedList<RowError>();
-    if (json != null) {
-      try {
-        JSONObject base = new JSONObject(json);
-        if (base.has("error") && base.has("reason")) {
-          String error = base.getString("error");
-          String reason = base.getString("reason");
-          errors.add(new RowError(error, reason));
-        }
-      } catch (JSONException e) {
-        errors.add(new RowError(Integer.toString(errorcode), "No reason given"));
-      }
-    }
-    return new ViewResponseWithDocs(rows, errors);
-  }
 }
