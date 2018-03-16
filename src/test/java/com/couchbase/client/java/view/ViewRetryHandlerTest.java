8d48dd5d624050b2fa6116b6d911566a655bc66d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:44 +0000

    First commited as before_44896_rev2

--- src/test/java/com/couchbase/client/java/view/ViewRetryHandlerTest.java
+++ src/test/java/com/couchbase/client/java/view/ViewRetryHandlerTest.java
@@ -67,7 +67,6 @@ public class ViewRetryHandlerTest {
 
         subscriber.awaitTerminalEvent(1, TimeUnit.SECONDS);
         subscriber.assertNoErrors();
-        assertEquals(1, subscriber.getOnNextEvents().size());
         assertEquals(1, subscriberCount.get());
     }
 
@@ -96,7 +95,6 @@ public class ViewRetryHandlerTest {
 
         subscriber.awaitTerminalEvent(1, TimeUnit.SECONDS);
         subscriber.assertNoErrors();
-        assertEquals(1, subscriber.getOnNextEvents().size());
         assertEquals(5, subscriberCount.get());
     }
 
@@ -151,6 +149,5 @@ public class ViewRetryHandlerTest {
         subscriber.assertNoErrors();
         assertEquals(404, subscriber.getOnNextEvents().get(0).responseCode());
         assertEquals(1, subscriberCount.get());
-        assertEquals(1, subscriber.getOnNextEvents().size());
     }
 }
