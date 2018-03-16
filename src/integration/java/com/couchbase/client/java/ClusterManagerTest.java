a99a33b7fbf8770fea2f44d8000d199796b4200a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:47 +0000

    First commited as after_40838_rev1

--- src/integration/java/com/couchbase/client/java/ClusterManagerTest.java
+++ src/integration/java/com/couchbase/client/java/ClusterManagerTest.java
@@ -1,9 +1,12 @@
 package com.couchbase.client.java;
 
+import com.couchbase.client.java.cluster.ClusterBucketSettings;
 import com.couchbase.client.java.cluster.ClusterInfo;
 import com.couchbase.client.java.cluster.ClusterManager;
+import com.couchbase.client.java.cluster.DefaultClusterBucketSettings;
 import com.couchbase.client.java.util.ClusterDependentTest;
 import org.junit.Test;
+import rx.functions.Action1;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -12,11 +15,44 @@ public class ClusterManagerTest extends ClusterDependentTest {
 
     @Test
     public void shouldLoadInfo() {
-        ClusterManager manager = cluster().clusterManager("Administrator", "password").toBlocking().single();
-        ClusterInfo info = manager.info().toBlocking().single();
+        ClusterInfo info = clusterManager().info().toBlocking().single();
 
         assertNotNull(info);
         assertTrue(info.raw().getObject("storageTotals").getObject("ram").getLong("total") > 0);
+    }
+
+    @Test
+    public void shouldGetBuckets() {
+        clusterManager().getBuckets().toBlocking().forEach(new Action1<ClusterBucketSettings>() {
+            @Override
+            public void call(ClusterBucketSettings clusterBucketSettings) {
+                System.err.println(clusterBucketSettings);
+            }
+        });
+    }
+
+    @Test
+    public void shouldRemoveBucket() {
+        // TODO: insert bucket first and then delete
+        Boolean done = clusterManager().removeBucket("foo").toBlocking().single();
+        assertTrue(done);
+        System.out.println(done);
+    }
+
+    @Test
+    public void shouldInsertBucket() {
+        ClusterBucketSettings settings = DefaultClusterBucketSettings
+            .builder()
+            .name("foobar")
+            .quota(512000)
+            .build();
+
+        clusterManager().insertBucket(settings).toBlocking().single();
+    }
+
+    @Test
+    public void shouldUpdateBucket() {
+
     }
 
 }
