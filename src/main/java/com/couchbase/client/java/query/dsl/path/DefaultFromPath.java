fac5c392a3723427bc476044fb251198bbc29c5d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:58 +0000

    First commited as before_45447_rev1

--- src/main/java/com/couchbase/client/java/query/dsl/path/DefaultFromPath.java
+++ src/main/java/com/couchbase/client/java/query/dsl/path/DefaultFromPath.java
@@ -1,6 +1,5 @@
 package com.couchbase.client.java.query.dsl.path;
 
-import com.couchbase.client.java.query.dsl.Expression;
 import com.couchbase.client.java.query.dsl.element.FromElement;
 
 /**
@@ -17,12 +16,6 @@ public class DefaultFromPath extends DefaultLetPath implements FromPath {
     @Override
     public AsPath from(String from) {
         element(new FromElement(from));
-        return new DefaultAsPath(this);
-    }
-
-    @Override
-    public AsPath from(Expression from) {
-        element(new FromElement(from.toString()));
         return new DefaultAsPath(this);
     }
 }
