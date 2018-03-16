3b6f458783079a5af6d01b845ef8741b3369efff
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:43:11 +0000

    First commited as before_82444_rev2

--- src/integration/java/com/couchbase/client/java/SubDocumentTest.java
+++ src/integration/java/com/couchbase/client/java/SubDocumentTest.java
@@ -59,12 +59,10 @@ import com.couchbase.client.java.error.subdoc.PathMismatchException;
 import com.couchbase.client.java.error.subdoc.PathNotFoundException;
 import com.couchbase.client.java.error.subdoc.SubDocumentException;
 import com.couchbase.client.java.subdoc.DocumentFragment;
-import com.couchbase.client.java.subdoc.LookupInBuilder;
 import com.couchbase.client.java.subdoc.MutateInBuilder;
 import com.couchbase.client.java.subdoc.SubdocOptionsBuilder;
 import com.couchbase.client.java.util.CouchbaseTestContext;
 import com.couchbase.client.java.util.features.CouchbaseFeature;
-import com.couchbase.client.java.util.features.Version;
 import org.assertj.core.api.Assertions;
 import org.junit.AfterClass;
 import org.junit.Assume;
@@ -1840,56 +1838,5 @@ public class SubDocumentTest {
 
         assertTrue(result.cas() != 0);
         assertEquals(ResponseStatus.SUCCESS, result.status("string"));
-    }
-
-    @Test
-    public void shouldSingleLookupGetCountOnArray() {
-        ctx.ignoreIfClusterUnder(new Version(5, 0 , 0));
-
-        DocumentFragment<Lookup> result = ctx.bucket()
-            .lookupIn(key)
-            .getCount("array")
-            .execute();
-
-        assertEquals(3L, result.content("array"));
-    }
-
-    @Test
-    public void shouldSingleLookupGetCountOnObject() {
-        ctx.ignoreIfClusterUnder(new Version(5, 0 , 0));
-
-        DocumentFragment<Lookup> result = ctx.bucket()
-            .lookupIn(key)
-            .getCount("sub")
-            .execute();
-
-        assertEquals(1L, result.content("sub"));
-    }
-
-    @Test
-    public void shouldMultiLookupGetCount() {
-        ctx.ignoreIfClusterUnder(new Version(5, 0 , 0));
-
-        DocumentFragment<Lookup> result = ctx.bucket()
-            .lookupIn(key)
-            .getCount("array")
-            .getCount("sub")
-            .execute();
-
-        assertEquals(3L, result.content("array"));
-        assertEquals(1L, result.content("sub"));
-    }
-
-    @Test
-    public void shouldFailSingleLookupGetCountOnPathError() {
-        ctx.ignoreIfClusterUnder(new Version(5, 0 , 0));
-
-        DocumentFragment<Lookup> result = ctx.bucket()
-            .lookupIn(key)
-            .getCount("does_not_exist")
-            .execute();
-
-        assertEquals(ResponseStatus.SUBDOC_PATH_NOT_FOUND, result.status("does_not_exist"));
-        assertNull(result.content("does_not_exist"));
     }
 }
