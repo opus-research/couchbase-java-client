3d29479c4ac68f0a9a9cb97ecc8cf9ebc4a4c097
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:34:30 +0000

    First commited as before_29848_rev4

--- src/main/java/com/couchbase/client/internal/ReplicaGetFuture.java
+++ src/main/java/com/couchbase/client/internal/ReplicaGetFuture.java
@@ -58,13 +58,29 @@ public class ReplicaGetFuture<T extends Object>
     this.completedFuture = new AtomicReference<GetFuture<T>>();
   }
 
+  /**
+   * Add a {@link GetFuture} to mointor.
+   *
+   * Note that this method is for internal use only.
+   *
+   * @param future the future to monitor.
+   */
   public void addFutureToMonitor(GetFuture<T> future) {
     this.monitoredFutures.add(future);
   }
 
+  /**
+   * Mark a monitored future as complete.
+   *
+   * Note that this method is for internal use only. It will also cancel
+   * all other registered futures.
+   *
+   * @param future the future to mark as completed.
+   */
   public void setCompletedFuture(GetFuture<T> future) {
-    notifyListeners();
     completedFuture.set(future);
+    cancelOtherFutures(completedFuture.get());
+    notifyListeners();
   }
 
   @Override
@@ -85,7 +101,6 @@ public class ReplicaGetFuture<T extends Object>
 
     while(System.currentTimeMillis() - start <= timeoutMs) {
       if (isDone() && !completedFuture.get().isCancelled()) {
-        cancelOtherFutures(completedFuture.get());
         return completedFuture.get().get();
       }
     }
@@ -94,7 +109,12 @@ public class ReplicaGetFuture<T extends Object>
       + "before timeout.");
   }
 
-  public void cancelOtherFutures(GetFuture successFuture) {
+  /**
+   * Cancel all other futures that are not completed.
+   *
+   * @param successFuture
+   */
+  private void cancelOtherFutures(GetFuture successFuture) {
     for(GetFuture future : monitoredFutures) {
       if(!future.equals(successFuture)) {
         future.cancel(true);
