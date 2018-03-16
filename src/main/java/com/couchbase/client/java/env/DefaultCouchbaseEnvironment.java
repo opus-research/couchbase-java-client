28f2d4c343727c24f982f1558b83d05d273f4ca9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:42:59 +0000

    First commited as before_80433_rev2

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -594,24 +594,6 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
-        public Builder continuousKeepAliveEnabled(boolean continuousKeepAliveEnabled) {
-            super.continuousKeepAliveEnabled(continuousKeepAliveEnabled);
-            return this;
-        }
-
-        @Override
-        public Builder keepAliveErrorThreshold(long keepAliveErrorThreshold) {
-            super.keepAliveErrorThreshold(keepAliveErrorThreshold);
-            return this;
-        }
-
-        @Override
-        public Builder keepAliveTimeout(long keepAliveTimeout) {
-            super.keepAliveTimeout(keepAliveTimeout);
-            return this;
-        }
-
-        @Override
         public DefaultCouchbaseEnvironment build() {
             return new DefaultCouchbaseEnvironment(this);
         }
