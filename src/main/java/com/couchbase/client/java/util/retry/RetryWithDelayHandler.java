e4f7473d59951336f8299a67417d3ddf7e18729e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:39:48 +0000

    First commited as before_60562_rev2

--- src/main/java/com/couchbase/client/java/util/retry/RetryWithDelayHandler.java
+++ src/main/java/com/couchbase/client/java/util/retry/RetryWithDelayHandler.java
@@ -31,7 +31,6 @@ import com.couchbase.client.core.time.ExponentialDelay;
 import com.couchbase.client.java.error.CannotRetryException;
 import rx.Observable;
 import rx.Scheduler;
-import rx.functions.Action4;
 import rx.functions.Func1;
 
 /**
@@ -51,9 +50,8 @@ public class RetryWithDelayHandler implements Func1<Tuple2<Integer, Throwable>, 
 
     protected final int maxAttempts;
     protected final Delay retryDelay;
-    protected final Func1<Throwable, Boolean> errorInterruptingPredicate;
+    protected final Func1<Throwable, Boolean> stoppingErrorFilter;
     protected final Scheduler optionalScheduler;
-    protected final Action4<Integer, Throwable, Long, TimeUnit> doOnRetry;
 
     /**
      * Construct a {@link RetryWithDelayHandler retry handler} that will retry on all errors.
@@ -64,7 +62,7 @@ public class RetryWithDelayHandler implements Func1<Tuple2<Integer, Throwable>, 
      *  eg. by using {@link ExponentialDelay}).
      */
     public RetryWithDelayHandler(int maxAttempts, Delay retryDelay) {
-        this(maxAttempts, retryDelay, null, null);
+        this(maxAttempts, retryDelay, null);
     }
 
     /**
@@ -74,24 +72,22 @@ public class RetryWithDelayHandler implements Func1<Tuple2<Integer, Throwable>, 
      *                    capped at <code>{@link Integer#MAX_VALUE} - 1</code>.
      * @param retryDelay the {@link Delay} to apply between each retry (can grow,
      *  eg. by using {@link ExponentialDelay}).
-     * @param errorInterruptingPredicate a predicate that determine if an error must stop the retry cycle (when true),
+     * @param stoppingErrorFilter a predicate that determine if an error must stop the retry cycle (when true),
      *  in which case said error is cascaded down.
      */
-    public RetryWithDelayHandler(int maxAttempts, Delay retryDelay, Func1<Throwable, Boolean> errorInterruptingPredicate,
-            Action4<Integer, Throwable, Long, TimeUnit> doOnRetry) {
-        this(maxAttempts, retryDelay, errorInterruptingPredicate, doOnRetry, null);
+    public RetryWithDelayHandler(int maxAttempts, Delay retryDelay, Func1<Throwable, Boolean> stoppingErrorFilter) {
+        this(maxAttempts, retryDelay, stoppingErrorFilter, null);
     }
 
     /**
      * Protected constructor that also allows to set a {@link Scheduler} for the delay, especially useful for tests.
      */
-    protected RetryWithDelayHandler(int maxAttempts, Delay retryDelay, Func1<Throwable, Boolean> errorInterruptingPredicate,
-            Action4<Integer, Throwable, Long, TimeUnit> doOnRetry, Scheduler scheduler) {
+    protected RetryWithDelayHandler(int maxAttempts, Delay retryDelay, Func1<Throwable, Boolean> stoppingErrorFilter,
+        Scheduler scheduler) {
         this.maxAttempts = Math.min(Integer.MAX_VALUE - 1, maxAttempts);
         this.retryDelay = retryDelay;
-        this.errorInterruptingPredicate = errorInterruptingPredicate;
+        this.stoppingErrorFilter = stoppingErrorFilter;
         this.optionalScheduler = scheduler;
-        this.doOnRetry = doOnRetry;
     }
 
     protected static String messageForMaxAttempts(long reachedAfterNRetries) {
@@ -100,21 +96,16 @@ public class RetryWithDelayHandler implements Func1<Tuple2<Integer, Throwable>, 
 
     @Override
     public Observable<?> call(Tuple2<Integer, Throwable> attemptError) {
-        final int errorNumber = attemptError.value1();
-        final Throwable error = attemptError.value2();
+        long errorNumber = attemptError.value1();
+        Throwable error = attemptError.value2();
 
         if (errorNumber > maxAttempts) {
             return Observable.error(new CannotRetryException(messageForMaxAttempts(errorNumber - 1), error));
-        } else if (errorInterruptingPredicate != null && errorInterruptingPredicate.call(error) == Boolean.TRUE) {
+        } else if (stoppingErrorFilter != null && stoppingErrorFilter.call(error) == Boolean.TRUE) {
             return Observable.error(error);
         } else {
-            final long delay = retryDelay.calculate(errorNumber);
-            final TimeUnit unit = retryDelay.unit();
-
-            if (doOnRetry != null) {
-                doOnRetry.call(errorNumber, error, delay, unit);
-            }
-
+            long delay = retryDelay.calculate(errorNumber);
+            TimeUnit unit = retryDelay.unit();
             if (this.optionalScheduler != null) {
                 return Observable.timer(delay, unit, optionalScheduler);
             } else {
