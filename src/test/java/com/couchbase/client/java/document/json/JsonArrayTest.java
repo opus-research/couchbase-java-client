691f0c66366d23cdd9ea7b8ca678653defb61a0b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:06 +0000

    First commited as before_45929_rev2

--- src/test/java/com/couchbase/client/java/document/json/JsonArrayTest.java
+++ src/test/java/com/couchbase/client/java/document/json/JsonArrayTest.java
@@ -21,6 +21,8 @@
  */
 package com.couchbase.client.java.document.json;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
@@ -34,8 +36,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.junit.Test;
 
 /**
  * Verifies the functionality provided by a {@link JsonArray}.
@@ -315,42 +315,4 @@ public class JsonArrayTest {
         }
     }
 
-    @Test
-    public void shouldConvertFromStringJson() {
-        String someJson = "[ 123, \"value\", true, [ 123 ], { \"sub\": \"obj\" }]";
-        JsonArray expected = JsonArray.create()
-                                        .add(123)
-                                        .add("value")
-                                        .add(true)
-                                        .add(JsonArray.from(123))
-                                        .add(JsonObject.create().put("sub", "obj"));
-
-        JsonArray converted = JsonArray.parse(someJson);
-
-        assertEquals(expected, converted);
-    }
-
-    @Test
-    public void shouldConvertNullStringToNull() {
-        assertNull(JsonArray.parse("null"));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldFailToConvertBadJsonString() {
-        String badJson = "This is not \"JSON\"!";
-        JsonObject.parse(badJson);
-    }
-
-    @Test
-    public void shouldFailToConvertNonArrayJson() {
-        String bad1 = "true";
-        String bad2 = "123";
-        String bad3 = "\"string\"";
-        String bad4 = "{\"some\": \"value\"}";
-
-        try { JsonArray.parse(bad1); fail(); } catch (IllegalArgumentException e) { }
-        try { JsonArray.parse(bad2); fail(); } catch (IllegalArgumentException e) { }
-        try { JsonArray.parse(bad3); fail(); } catch (IllegalArgumentException e) { }
-        try { JsonArray.parse(bad4); fail(); } catch (IllegalArgumentException e) { }
-    }
 }
