232552435b2b153e92d55ac1fef789ba17582698
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:41:31 +0000

    First commited as before_68904_rev2

--- src/main/java/com/couchbase/client/java/AsyncBucket.java
+++ src/main/java/com/couchbase/client/java/AsyncBucket.java
@@ -24,7 +24,6 @@ import com.couchbase.client.core.RequestCancelledException;
 import com.couchbase.client.core.annotations.InterfaceAudience;
 import com.couchbase.client.core.annotations.InterfaceStability;
 import com.couchbase.client.java.bucket.AsyncBucketManager;
-import com.couchbase.client.java.datastructures.MutationOptionBuilder;
 import com.couchbase.client.java.document.BinaryDocument;
 import com.couchbase.client.java.document.Document;
 import com.couchbase.client.java.document.JsonDocument;
@@ -2049,114 +2048,6 @@ public interface AsyncBucket {
     @InterfaceStability.Committed
     @InterfaceAudience.Public
     AsyncMutateInBuilder mutateIn(String docId);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <V>Observable<V> mapGet(String docId, String key, Class<V> valueType);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <V>Observable<Boolean> mapAdd(String docId, String key, V value);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <V>Observable<Boolean> mapAdd(String docId, String key, V value, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <V>Observable<V> mapRemove(String docId, String key, Class<V> valueType);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <V>Observable<V> mapRemove(String docId, String key, Class<V> valueType, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Integer> mapSize(String docId);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<E> listGet(String docId, int index, Class<E> elementType);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listPush(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listPush(String docId, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listShift(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listShift(String docId, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Boolean> listRemove(String docId, int index);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Boolean> listRemove(String docId, int index, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listSet(String docId, int index, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> listSet(String docId, int index, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Integer> listSize(String docId);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> setAdd(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> setAdd(String docId, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> setExists(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> setRemove(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> setRemove(String docId, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Integer> setSize(String docId);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> queueAdd(String docId, E element);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<Boolean> queueAdd(String docId, E element, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<E> queueRemove(String docId, Class<E> elementType);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    <E>Observable<E> queueRemove(String docId, Class<E> elementType, MutationOptionBuilder mutationOptionBuilder);
-
-    @InterfaceStability.Experimental
-    @InterfaceAudience.Public
-    Observable<Integer> queueSize(String docId);
 
     /**
      * Invalidates and clears the internal query cache.
