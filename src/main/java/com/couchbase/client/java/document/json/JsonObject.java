3e1a6dd4e7e450ced136ed096d2cd24c6020effc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:20 +0000

    First commited as before_37960_rev5

--- src/main/java/com/couchbase/client/java/document/json/JsonObject.java
+++ src/main/java/com/couchbase/client/java/document/json/JsonObject.java
@@ -115,21 +115,6 @@ public class JsonObject implements JsonValue {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("{");
-        int size = content.size();
-        int item = 0;
-        for(Map.Entry<String, Object> entry : content.entrySet()) {
-            sb.append("\"").append(entry.getKey()).append("\":");
-            if (entry.getValue() instanceof String) {
-                sb.append("\"").append(entry.getValue()).append("\"");
-            } else {
-                sb.append(entry.getValue().toString());
-            }
-            if (++item < size) {
-                sb.append(",");
-            }
-        }
-        sb.append("}");
-        return sb.toString();
+        return content.toString();
     }
 }
