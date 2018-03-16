974586723fbf3f977a6ac6d2784c520cfb7eeed4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:35:59 +0000

    First commited as before_41484_rev1

--- src/main/java/com/couchbase/client/java/document/Document.java
+++ src/main/java/com/couchbase/client/java/document/Document.java
@@ -21,8 +21,6 @@
  */
 package com.couchbase.client.java.document;
 
-import com.couchbase.client.core.annotations.InterfaceAudience;
-import com.couchbase.client.core.annotations.InterfaceStability;
 import com.couchbase.client.java.AsyncBucket;
 
 /**
@@ -31,8 +29,6 @@ import com.couchbase.client.java.AsyncBucket;
  * @author Michael Nitschinger
  * @since 2.0
  */
-@InterfaceStability.Committed
-@InterfaceAudience.Public
 public interface Document<T> {
 
    /**
