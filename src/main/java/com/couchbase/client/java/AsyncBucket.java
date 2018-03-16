fd85d6c6d08d5f0f938903df2cfa3e12e4119c18
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:36:10 +0000

    First commited as before_42155_rev2

--- src/main/java/com/couchbase/client/java/AsyncBucket.java
+++ src/main/java/com/couchbase/client/java/AsyncBucket.java
@@ -48,9 +48,6 @@ import java.util.concurrent.TimeUnit;
  * integration point and the application. Networks are unreliable, servers can fail and the SDK contains bugs. With applying
  * a timeout and reacting to them accordingly, application level code is less likely to fail.
  *
- * Note that a {@link #isClosed() closed} Bucket will throw {@link com.couchbase.client.java.error.BucketClosedException}
- * if any more attempts to use it are made.
- *
  * @author Michael Nitschinger
  * @since 2.0
  */
@@ -1109,10 +1106,4 @@ public interface AsyncBucket {
      */
     Observable<Boolean> close();
 
-    /**
-     * Checks the closed status of the bucket.
-     *
-     * @return true if the AsyncBucket has previously been successfully {@link #close() closed}
-     */
-    boolean isClosed();
 }
--- src/main/java/com/couchbase/client/java/Bucket.java
+++ src/main/java/com/couchbase/client/java/Bucket.java
@@ -50,9 +50,6 @@ import java.util.concurrent.TimeoutException;
  * Default timeouts are always applied and can be configured through the {@link CouchbaseEnvironment}. Overloads
  * are also available to change them on a per-call basis.
  *
- * Note that a {@link #isClosed() closed} Bucket will throw {@link com.couchbase.client.java.error.BucketClosedException}
- * if any more attempts to use it are made.
- *
  * @author Michael Nitschinger
  * @since 2.0
  */
@@ -64,7 +61,6 @@ public interface Bucket {
      * Provides access to the underlying asynchronous bucket interface.
      *
      * @return the asynchronous bucket.
-     * @throws com.couchbase.client.java.error.BucketClosedException if the bucket has previously been closed
      */
     AsyncBucket async();
 
@@ -2355,10 +2351,4 @@ public interface Bucket {
      */
     Boolean close(long timeout, TimeUnit timeUnit);
 
-    /**
-     * Checks the closed status of the bucket.
-     *
-     * @return true if the Bucket has previously been successfully {@link #close() closed}
-     */
-    boolean isClosed();
 }
