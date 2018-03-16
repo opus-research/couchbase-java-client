7ee873b5107dcd65fb20e04640760b4c112bf103
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 11:37:27 +0000

    First commited as before_48311_rev2

--- src/integration/java/com/couchbase/client/java/KeyValueTest.java
+++ src/integration/java/com/couchbase/client/java/KeyValueTest.java
@@ -32,6 +32,7 @@ import com.couchbase.client.java.error.DocumentAlreadyExistsException;
 import com.couchbase.client.java.error.DocumentDoesNotExistException;
 import com.couchbase.client.java.error.DurabilityException;
 import com.couchbase.client.java.error.RequestTooBigException;
+import com.couchbase.client.java.error.TemporaryFailureException;
 import com.couchbase.client.java.error.TemporaryLockFailureException;
 import com.couchbase.client.java.util.ClusterDependentTest;
 import org.junit.Assume;
@@ -40,6 +41,7 @@ import org.junit.Test;
 import java.io.Serializable;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -148,9 +150,6 @@ public class KeyValueTest extends ClusterDependentTest {
         assertTrue(doc1.cas() != doc2.cas());
         assertTrue(doc1.cas() != doc3.cas());
         assertTrue(doc2.cas() != doc3.cas());
-
-        JsonLongDocument doc4 = bucket().get("incr-key", JsonLongDocument.class);
-        assertEquals(20L, (long) doc4.content());
     }
 
     @Test
@@ -167,51 +166,6 @@ public class KeyValueTest extends ClusterDependentTest {
         assertTrue(doc1.cas() != doc2.cas());
         assertTrue(doc1.cas() != doc3.cas());
         assertTrue(doc2.cas() != doc3.cas());
-
-        JsonLongDocument doc4 = bucket().get("decr-key", JsonLongDocument.class);
-        assertEquals(80L, (long) doc4.content());
-    }
-
-    @Test
-    public void shouldIncrAndDecrAfterInitialUpsert() throws Exception {
-        String id = "incrdecr-key";
-        JsonLongDocument doc1 = bucket().upsert(JsonLongDocument.create(id, 30L));
-        assertEquals(30L, (long) doc1.content());
-
-        JsonLongDocument doc2 = bucket().get(id, JsonLongDocument.class);
-        assertEquals(30L, (long) doc2.content());
-        assertEquals(doc1.cas(), doc2.cas());
-
-        JsonLongDocument doc3 = bucket().counter(id, 10);
-        assertEquals(40L, (long) doc3.content());
-        assertTrue(doc3.cas() != doc2.cas());
-
-        JsonLongDocument doc4 = bucket().get(id, JsonLongDocument.class);
-        assertEquals(40L, (long) doc4.content());
-        assertEquals(doc4.cas(), doc3.cas());
-
-        JsonLongDocument doc5 = bucket().counter(id, -20);
-        assertEquals(20L, (long) doc5.content());
-        assertTrue(doc5.cas() != doc4.cas());
-
-        JsonLongDocument doc6 = bucket().get(id, JsonLongDocument.class);
-        assertEquals(20L, (long) doc6.content());
-        assertEquals(doc6.cas(), doc5.cas());
-    }
-
-    @Test
-    public void shouldHaveCounterInitialZero() throws Exception {
-        JsonLongDocument doc1 = bucket().counter("defincr-key", 10);
-        assertEquals(0, (long) doc1.content());
-
-        JsonLongDocument doc2 = bucket().get("defincr-key", JsonLongDocument.class);
-        assertEquals(0, (long) doc2.content());
-
-        JsonLongDocument doc3 = bucket().counter("defdecr-key", -10);
-        assertEquals(0, (long) doc3.content());
-
-        JsonLongDocument doc4 = bucket().get("defdecr-key", JsonLongDocument.class);
-        assertEquals(0, (long) doc4.content());
     }
 
     @Test
